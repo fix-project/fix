adee073b60c903570bb174baa86591a32a0d4b01
yhdengh <yhdeng@stanford.edu>
2020-12-01 13:49:58 -0800

    Added blob constructors and parsing.

--- src/component/blob.hh
+++ src/component/blob.hh
@@ -1,29 +1,39 @@
+#include <string>
+
+#include "file_descriptor.hh"
+#include "parser.hh"
+#include "spans.hh"
+
 class Blob {
   private:
     // Name of the blob
-    char name[32];
+    std::string name_;
     // Content of the blob
-    byte *content;
+    std::string content_;
+    // Corresponding file descriptor
+    int fd_;
+    // File descriptor wrapper
+    FileDescriptor fd_wrapper_;
 
-    // Memory instance of the file
     // TODO: If input are read-only, and only one output exists for a pair of program and input, there should not be concurrency issue for having one memory_instance. May become an issue if executions can take place on multiple cores in parallel???
-    void * memory_instance;
+    
     // Whether cashed in memory
-    bool memory_cached;
+    bool memory_cached_;
 
   public: 
     // Constructor
     Blob() {};
-    Blob(char name[32]) : name(name) {}; 
-
-    // Destructor
-    ~Blob();
+    Blob( std::string name ); 
 
     // Flush the file and computation tree 
     void flush();
 
     // Load the file into memory
-    void *load_to_memory();
+    void load_to_memory();
+
+    uint8_t serialized_length() const;
+    void serialize( Serializer& s ) const;
+    void parse( Parser& p );
 } 
 
 
