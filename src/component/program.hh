71a5dd853e158465da429b12c85b6f68aa47ad4b
yhdengh <yhdeng@stanford.edu>
2020-12-01 13:50:23 -0800

    Added program parsing

--- src/component/program.hh
+++ src/component/program.hh
@@ -1,4 +1,7 @@
-using namespace std;
+#include <string>
+
+#include "parser.hh"
+#include "spans.hh"
 
 /**
  * Structure of program files:
@@ -13,11 +16,6 @@ using namespace std;
  *        Wasm module
  * --------------------------------
  */
-
-typedef struct program_file_header {
-  uint64_t num_input_entry;
-  uint64_t num_output_entry;
-} ProgFileHdr;
 
 typedef struct input_entry {
   char[256] symbol;
@@ -34,40 +32,46 @@ typedef struct p_mem {
   void *data;
   // Entry point of the program
   void *entry_point;
-} ProogramMem;
+} ProgramMem;
 
 class Program {
   private:
     // Name of the program
-    char name[32];
+    std::string name_;
     // Content of the program
-    byte *content;
+    std::string content_;
+    // Corresponding file descriptor
+    int fd_;
+    // File descriptor wrapper
+    FileDescriptor fd_wrapper_;
+    
+    uint64_t num_input_entry_;
+    uint64_t num_output_entry_;
     // List of named input symbols
-    InputEntry * inputs;
+    span_view<InputEntry> inputs_;
     // List of named output symbols
-    OutputEntry * outputs;
-     
+    span_view<OutputEntry> outputs_;
+
     // Path of the wasm source code
-    const char* wasm_path;
+    const char* wasm_path_;
     // Whether the wasm source code has been cached
-    bool wasm_cached;
+    bool wasm_cached_;
 
     // Pointer to memory instance of the program
-    ProgramMem * memory_instance;
+    ProgramMem * memory_instance_;
     // Whether the program is in memory
-    bool memory_cached;
+    bool memory_cached_;
 
   public:
     // Default constructor
     Program() {};
-    Program(char name[32]) : name(name) {};
-
-    // Destructor
-    ~Program();
+    Program( string name ) : name_( name );
 
     // Load the program into memory
-    Program_mem *load_to_memory();
+    void load_to_memory();
 
     // Remove the program from memory
     void clear_from_memory();
+
+    void parse( Parser& p );
 }
