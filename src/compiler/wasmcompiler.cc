6b82ffe7f616870b6dc4f3f8ba8437e5e1fafffd
yhdengh <yhdeng@stanford.edu>
2020-12-11 10:26:14 -0800

    Added wasm to c compiler.

---
+++ src/compiler/wasmcompiler.cc
@@ -0,0 +1,117 @@
+#include "wasmcompiler.hh"
+
+#include "src/apply-names.h"
+#include "src/binary-reader.h"
+#include "src/binary-reader-ir.h"
+#include "src/error-formatter.h"
+#include "src/feature.h"
+#include "src/generate-names.h"
+#include "src/ir.h"
+#include "src/option-parser.h"
+#include "src/validator.h"
+#include "src/wast-lexer.h"
+
+#include "src/c-writer.h"
+
+using namespace std;
+using namespace wabt;
+
+namespace wasmcompiler 
+{
+  
+  static int s_verbose;
+  static std::string s_infile;
+  static std::string s_outfile;
+  static Features s_features;
+  static WriteCOptions s_write_c_options;
+  static bool s_read_debug_names = true;
+  static std::unique_ptr<FileStream> s_log_stream;
+  
+  MemoryStringStream::MemoryStringStream( Stream * log_stream ) : Stream( log_stream ) {}
+
+  string && MemoryStringStream::ReleaseStringBuffer 
+  {
+    return move(buf_);
+  }
+
+  Result MemoryStringStream::WriteDataImpl( size_t dst_offset, const void * src, size_t size ) 
+  {
+    if ( size == 0 ) {
+      return Result::Ok;
+    }
+    size_t end = dst_offset + size;
+    if ( end > buf_.size() ) {
+      buf_.resize( end );
+    }
+    uint8_t* dst = &buf_[ dst_offset ];
+    memcpy( dst, src, size );
+    return Result::Ok;
+  }
+
+  Result MemoryStringStream::MoveDataImpl( size_t dst_offset, size_t src_offset, size_t size ) 
+  {
+    if ( size == 0 ) {
+      return Result::Ok;
+    }
+    size_t src_end = src_offset + size;
+    size_t dst_end = dst_offset + size;
+    size_t end = src_end > dst_end ? src_end : dst_end;
+    if ( end > buf_.size() ) {
+      buf_.resize( end );
+    }
+
+    uint8_t* dst = &buf_[ dst_offset ];
+    uint8_t* src = &buf_[ src_offset ];
+    memmove( dst, src, size );
+    return Result::Ok;
+  }
+
+  Result MemoryStringStream::TruncateImpl( size_t size ) {
+    if ( size > buf_.size() ) {
+      return Result::Error;
+    }
+    buf_.resize( size );
+    return Result::Ok;
+  }
+
+  pair<string, string> wasm_to_c( const string & wasm_name, const string & wasm_content )
+  {
+    Result result;
+
+    Errors errors;
+    Module module;
+    const bool kStopOnFirstError = true;
+    const bool kFailOnCustomSectionError = true;
+    ReadBinaryOptions options( s_features, s_log_stream.get(), s_read_debug_names, 
+                               kStopOnFirstError,
+                               kFailOnCustomSectionError );
+    result = ReadBinaryIr( wasm_name.c_str(), wasm_content.data(), wasm_content.size(),
+                           options, &errors, &module );
+    if ( Succeeded( result ) ) {
+      if ( Succeeded( result ) ) {
+        ValidateOptions options( s_features );
+        result = ValidateModule( &module, &errors, options );
+        result |= GenerateNames( &module );
+      }
+
+      if ( Succeeded( result ) ) {
+        /* TODO(binji): This shouldn't fail; if a name can't be applied
+         * (because the index is invalid, say) it should just be skipped. */
+        Result dummy_result = ApplyNames( &module );
+        WABT_USE( dummy_result );
+      }
+
+      if ( Succeeded( result ) ) {
+        MemoryStringStream c_stream();
+        MemoryStringStream h_stream();
+        result = WriteC( &c_stream, &h_stream, "wasm.h", &module, s_write_c_options );
+      }
+    }
+    FormatErrorsToFile(errors, Location::Type::Binary);
+    if ( result != Result::Ok ) 
+    {
+      //TODO: throw exception
+    }
+    return { c_stream.Buf(), h_stream.Buf() } ;
+  }
+}
---
+++ src/compiler/wasmcompiler.hh
@@ -0,0 +1,28 @@
+#include <string>
+
+#include "src/stream.h"
+
+namespace wasmcompiler 
+{
+  class MemoryStringStream : public wabt::Stream
+  {
+    public:
+      WABT_DISALLOW_COPY_AND_ASSIGN( MemoryStringStream );
+      explicit MemoryStringStream( Stream* log_stream = nullptr );
+
+      std::string && ReleaseStringBuf();
+      std::string Buf() { return buf_; }
+    protected:
+      Result WriteDataImpl(size_t offset, const void* data, size_t size) override;
+      Result MoveDataImpl(size_t dst_offset,
+          size_t src_offset,
+          size_t size) override;
+      Result TruncateImpl(size_t size) override;
+
+    private:
+      std::string buf_;
+  }
+  
+  std::pair<std::string, std::string> wasm_to_c( const std::string & wasm_content );
+
+}
