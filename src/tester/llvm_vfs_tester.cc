0d91a515b6a3b838d261c18710dba90b536abed7
yhdengh <yhdeng@stanford.edu>
2020-12-17 14:38:41 -0800

    Overlay InMemoryFileSystem over read file system.

--- src/tester/llvm_vfs_tester.cc
+++ src/tester/llvm_vfs_tester.cc
@@ -6,15 +6,27 @@
 using namespace std;
 using namespace llvm;
 
-int main()
+int main( int argc, char * argv[] )
 {
   string file_content ("hello123456");
 
-  vfs::InMemoryFileSystem FS;
-  FS.addFile( "/hello", 0, MemoryBuffer::getMemBuffer( file_content ) );
-  auto File = FS.openFileForRead("/hello");
+  IntrusiveRefCntPtr<vfs::OverlayFileSystem> Base( new vfs::OverlayFileSystem( vfs::getRealFileSystem() ) );
+  IntrusiveRefCntPtr<vfs::InMemoryFileSystem> FS( new vfs::InMemoryFileSystem() );
+  FS->addFile( "/hello", 0, MemoryBuffer::getMemBuffer( file_content ) );
+  auto File = FS->openFileForRead("/hello");
 
   cout << ( string )( ( *( *File )->getBuffer( "hello" ) )->getBuffer() ) << endl;
+
+  Base->pushOverlay( FS );
+  File = Base->openFileForRead("/hello");
+  cout << ( string )( ( *( *File )->getBuffer( "hello" ) )->getBuffer() ) << endl;
+ 
+  if ( argc >= 2 ) 
+  {
+    cout << argv[1] << endl;
+    auto diskFile = Base->openFileForRead( argv[1] );
+    cout << ( string )( ( *( *diskFile )->getBuffer( "ignore" ) )->getBuffer() ) << endl;
+  }
 
   return 0;
 }
