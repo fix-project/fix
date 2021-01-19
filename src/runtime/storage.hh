1df6d3c2cd2a2a3039dda8f00a757780fc8a4af6
Yuhan Deng <yhdeng@stanford.edu>
2021-01-19 10:04:52 -0800

    Renamed runtime to storage

---
+++ src/runtime/storage.hh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <string_view>
+
+#include "blob.hh"
+#include "encoded_blob.hh"
+#include "encode.hh"
+#include "program.hh"
+
+class Storage {
+  private:
+    // Map from name to content (blob / encoded blob)
+    std::map<std::string, Blob> name_to_blob_;
+    // Map from name to content of encoded blob
+    std::map<std::string, EncodedBlob> name_to_encoded_blob_;
+    // Map from name to encode
+    std::map<std::string, Encode> name_to_encode_;
+    // Map from name to program
+    std::map<std::string, Program> name_to_program_;
+
+    Storage ()
+      : name_to_blob_(),
+        name_to_encoded_blob_(),
+        name_to_encode_(),
+        name_to_program_() 
+    {}
+
+  public:
+    // Return reference to blob content
+    std::string_view getBlob( const std::string & name );
+
+    // Return reference to encoded blob content
+    std::string & getEncodedBlob( const std::string & name );
+
+    static Storage & getInstance () 
+    {
+      static Storage runtime_instance;
+      return runtime_instance;
+    }
+};
