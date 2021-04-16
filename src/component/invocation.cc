da77783448c64fea1a41b92664eb1110e3eab677
Yuhan Deng <yhdeng@stanford.edu>
2021-04-16 14:05:06 -0700

    Updated invocation design,

--- src/component/invocation.cc
+++ src/component/invocation.cc
@@ -4,30 +4,133 @@
 
 using namespace std;
 
-WasmFileDescriptor & Invocation::getFd( int fd_id )
-{
-  return id_to_fd_.at( fd_id );
-}
-
-int Invocation::addFd( int fd_id, WasmFileDescriptor fd )
-{
-  id_to_fd_.try_emplace( fd_id, fd );
-  return fd_id;
-}
-
-int Invocation::openVariable( const size_t & index )
-{
-  // If the index is greater than the range of inputs
-  if ( index >= input_count_ )
-  {
-    id_to_fd_.try_emplace( index, WasmFileDescriptor() );
-  }
-  else 
-  {
-    // TODO: look for the input in input lists
-    id_to_fd_.try_emplace( index, WasmFileDescriptor( getInputName( index ) ) ); 
-  }
-  return index;
-}
-
 uint64_t Invocation::next_invocation_id_ = 0;
+
+void Invocation::attach_input( uint32_t input_index, uint32_t mem_index )
+{
+  if ( input_index >= input_count_ )
+  {
+    throw out_of_range ( "Input does not exist." );
+  }
+
+  Name strict_input = RuntimeStorage::getInstance().getTree( encode_name_ ).at(1);
+  Name input_name = RuntimeStorage::getInstance().getTree( strict_input ).at( input_index );
+
+  input_mems.insert_or_assign( mem_index, InputMem( input_name ) );
+}
+
+template<typename T>
+uint32_t Invocation::get_i32( uint32_t mem_index, uint32_t ofst )
+{
+  switch ( input_mems.at( mem_index ).name_.getContentType() )
+  {
+    case ContentType::Blob :
+     uint32_t res = *( T* )RuntimeStorage::getInstance().getBlob( input_mems.at( mem_index ).name_ )[ ofst ];
+     return res;
+
+    default :
+     throw runtime_error ( "No write access." );
+  }
+}
+
+void Invocation::attach_output( uint32_t output_index, uint32_t mem_index, uint32_t output_type )
+{
+  if ( output_index >= output_tree_.childs.size() )
+  {
+    output_tree_.childs.resize( output_index + 1 );
+  }
+
+  output_tree_.childs.emplace( output_tree_.childs.begin() + ouptut_index, output_type );
+  output_mems.insert_or_assign( mem_index, &output_tree_.childs.at( output_index ) );
+
+  return;
+}
+
+void Invocation::attach_output_child( uint32_t parent_mem_index, uint32_t child_index, uint32_t child_mem_index, uint32_t output_type )
+{ 
+  OutputTemp * parent_mem = output_mems.at( parent_mem_index );
+  
+  switch ( parent_mem->name_.getContentType() )
+  {
+    case ContentType::Blob :
+    case ContentType::Thunk :
+      throw runtime_error ( "Cannot add child to non-Tree output." );
+
+    default :
+      continue;
+  }
+  
+  if ( child_index >= parent_mem->childs.size() )
+  {
+    parent_mem->childs.resize( child_index + 1 );
+  }
+
+  parent_mem->childs.emplace( parent_mem->childs.begin + child_index, output_type );
+  output_mems.insert_or_assign( child_mem_index, parent_mem->childs.at( child_index ));
+
+  return;
+}
+
+template<typename T>
+uint32_t Invocation::store_i32( uint32_t mem_index, uint32_t content )
+{
+  OutputTemp * output_mem = output_mems.at( mem_index );
+  switch ( output_mem->name_.getContentType() )
+  {
+    case ContentType::Blob :
+     T content_T = ( T )content; 
+     ouptut_mem.buffer.append( (char *)&content_T, sizeof( T )  );
+     return;
+
+    default:
+     throw runtime_error ( "Cannot write to non-Blob output." ); 
+  }
+}
+
+void Invocation::set_encode( uint32_t mem_index, uint32_t encode_mem_index )
+{
+  OutputTemp * output_mem = output_mems.at( mem_index );
+  
+  switch ( output_mem->name_.getContentType() )
+  {
+    case ContentType::Blob :
+    case ContentType::Tree :
+      throw runtime_error ( "Cannot set encode to a non-Thunk output." );
+      
+    default :
+      continue;
+  }
+
+  OutputTemp * encode_mem = output_mems.at( encode_mem_index );
+
+  switch ( encode_mem_index->name_.getContentType() )
+  {
+    case ContentType::Tree :
+      output_mem->encode = encode_mem;
+      return;
+
+    default :
+      throw runtime_error ( "Cannot set Blob or Thunk output as encode." );
+  }
+}
+
+void Invocation::add_path( uint32_t mem_index, uint32_t path )
+{
+  OutputTemp * output_mem = output_mems.at( mem_index );
+  
+  switch ( output_mem->name_.getContentType() )
+  {
+    case ContentType::Blob :
+    case ContentType::Tree :
+      throw runtime_error ( "Cannot set encode to a non-Thunk output." );
+      
+    default :
+      continue;
+  }
+
+  output_mem->path.push_back( static_cast<int>( path ) );
+  return;
+}
+  
+
+      
--- src/component/invocation.hh
+++ src/component/invocation.hh
@@ -8,24 +8,76 @@
 #include "name.hh"
 #include "wasm-rt.h"
 
-struct WasmFileDescriptor 
-{
-  Name blob_name_;
-  uint64_t loc_;
-  std::string buffer;
+#define BLOB 0
+#define TREE 1
+#define THUNK 2
 
-  WasmFileDescriptor( Name blob_name )
-    : blob_name_( blob_name ),
-      loc_( 0 ),
-      buffer( "" )
+struct InputMem
+{
+  Name name_;
+
+  InputMem( Name name )
+    : name_( name )
+  {}
+};
+
+struct OutputTemp
+{
+  Name name_;
+  
+  std::vector<OutputTemp> childs;
+  std::string buffer;
+  OutputTemp* encode;
+  std::vector<int> path;
+ 
+  OutputTemp( Name name )
+    : name_( name ),
+      childs(),
+      buffer( "" ),
+      encode(),
+      path()
   {}
 
-  WasmFileDescriptor()
-    : blob_name_(),
-      loc_( 0 ),
-      buffer( "" )
-  {} 
+  OutputTemp( ContentType content_type )
+    : name_( content_type ),
+      childs(),
+      buffer( "" ),
+      encode(),
+      path()
+  {}
 
+  OutputTemp( uint32_t content_type )
+    : name_(),
+      childs(),
+      buffer( "" ),
+      encode(),
+      path()
+  {
+    switch ( content_type ) 
+    {
+      case BLOB : 
+        name_ = Name ( ContentType::Blob );
+
+      case TREE :
+        name_ = Name ( ContentType::Tree );
+
+      case THUNK :
+        name_ = Name ( ContentType::Thunk );
+        
+      default :
+        throw std::runtime_error ( "Invalid argument." );
+    }
+  }
+
+  OutputTemp( const OutputTemp & temp )
+    : name_( temp.name_ ),
+      childs( temp.childs ),
+      buffer( temp.buffer ),
+      encode( temp.encode ),
+      path( temp.path )
+  {}
+
+  OutputTemp& operator=( const OutputTemp& temp ) = default;
 };
 
 class Invocation {
@@ -39,8 +91,14 @@ class Invocation {
     // Corresponding memory instance
     wasm_rt_memory_t *mem_;
 
-    // Map from fd id to actual fd
-    absl::flat_hash_map<int, WasmFileDescriptor> id_to_fd_;
+    // Map from id to input mem
+    absl::flat_hash_map<uint32_t, InputMem> input_mems;
+
+    // Temporary output tree
+    OutputTemp output_tree_;
+
+    // Map from id to output mems
+    absl::flat_hash_map<uint32_t, OutputTemp*> output_mems;
 
     // vector of the number of strict inputs/lazy inputs
     std::vector<size_t> num_inputs_;
@@ -53,7 +111,9 @@ class Invocation {
       : program_name_( program_name ),
         encode_name_( encode_name ),
         mem_( mem ),
-        id_to_fd_(),
+        input_mems(),
+        output_tree_( ContentType::Tree ),
+        output_mems(),
         num_inputs_(),
         input_count_( 0 )
     {}
@@ -62,7 +122,9 @@ class Invocation {
       : program_name_( "" ),
         encode_name_( encode_name ),
         mem_( 0 ),
-        id_to_fd_(),
+        input_mems(),
+        output_tree_( ContentType::Tree ),
+        output_mems(),
         num_inputs_(),
         input_count_( 0 )
     {}
@@ -75,15 +137,6 @@ class Invocation {
 
     // Return pointer to wasm memory
     wasm_rt_memory_t * getMem() { return mem_; }
-
-    // Return file descriptor corresponding to fd_id
-    WasmFileDescriptor & getFd( int fd_id );
-
-    // Add file descriptor, return corresponding fd_id
-    int addFd( int fd_id, WasmFileDescriptor fd );
-
-    // Open blob corresponding to variable name
-    int openVariable( const size_t & index );
     
     static uint64_t next_invocation_id_;
 
@@ -97,5 +150,21 @@ class Invocation {
     Name getInputName( const size_t & index );
 
     size_t getInputCount() const { return input_count_; }
+
+    void attach_input( uint32_t input_index, uint32_t mem_index );
+
+    template<typename T>
+    uint32_t get_i32( uint32_t mem_index, uint32_t ofst );
+    
+    void attach_output( uint32_t output_index, uint32_t mem_index, uint32_t output_type ); 
+
+    void attach_output_child ( uint32_t parent_mem_index, uint32_t child_index, uint32_t child_mem_index, uint32_t output_type );
+
+    template<typename T>
+    uint32_t store_i32( uint32_t mem_index, uint32_t content );
+
+    void set_encode( uint32_t mem_index, uint32_t encode_index );
+
+    void add_path( uint32_t mem_index, uint32_t path );
 };
     
