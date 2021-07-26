6fcfa8cba3367ef459d9c7ece14a89fef08f990d
Yuhan Deng <yuhan@puffer5.stanford.edu>
2021-07-26 08:16:55 -0700

    Update wasi.c

--- code_example/wasi/wasi.c
+++ code_example/wasi/wasi.c
@@ -1,12 +1,20 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "api.h"
 
 extern void attach_input(uint32_t input_index, uint32_t mem_index);
-extern void get_int(uint32_t mem_index, uint32_t ofst);
+extern uint32_t get_int(uint32_t mem_index, uint32_t ofst);
+extern uint8_t get_int8(uint32_t mem_index, uint32_t ofst);
 
-__wasi_errno_t __wasi_path_open(
+extern uint8_t mem_store(uint32_t offset, uint8_t content);
+extern uint8_t mem_load(uint32_t offset, uint8_t content);
+
+uint32_t fd_ofst [5];
+uint32_t next_fd_idx = 0;
+
+__wasi_errno_t __my_wasi_path_open(
     __wasi_fd_t fd,
     /**
      * Flags determining the method of how the path is resolved.
@@ -36,10 +44,13 @@ __wasi_errno_t __wasi_path_open(
     __wasi_fd_t *retptr0
 ) 
 {
-  return 0;
+  uint32_t input_index = (uint32_t)(atoi(path));
+  attach_input(input_index, next_fd_idx);
+  next_fd_idx += 1;
+  return next_fd_idx;
 }
 
-__wasi_errno_t __wasi_fd_read(
+__wasi_errno_t __my_wasi_fd_read(
     __wasi_fd_t fd,
     /**
      * List of scatter/gather vectors to which to store data.
@@ -50,7 +61,17 @@ __wasi_errno_t __wasi_fd_read(
      */
     size_t iovs_len,
     __wasi_size_t *retptr0
-) 
+)__attribute__((export_name("my_wasi_fd_read")))
+{
+  for( int i = 0; i < iovs_len; i++ )
+  {
+    iovs->buf[i] = get_int8(fd, fd_ofst[fd]);
+    fd_ofst[fd]++;
+  }
+  return iovs_len;  
+}
+
+int main() 
 {
   return 0;
 }
