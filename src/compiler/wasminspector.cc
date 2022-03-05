41c2ac81674b1d77bfbe603aa07c0e9606eec882
Yuhan Deng <yhdeng@stanford.edu>
2022-03-04 17:25:06 -0800

    Added wasminspector
    * Validate that any memory exported as ro is not written to, or exported as rw
    * Extract list of ro memories, rw memories, and imported functinos

---
+++ src/compiler/wasminspector.cc
@@ -0,0 +1,146 @@
+#include "wasminspector.hh"
+
+#include <string>
+
+using namespace std;
+
+namespace wasminspector {
+
+using namespace wabt;
+
+WasmInspector::WasmInspector( Module* module, Errors* errors )
+  : errors_( errors )
+  , current_module_( module )
+  , visitor_( this )
+  , imported_functions_()
+  , exported_ro_()
+  , exported_rw_()
+{
+  for ( Export* export_ : module->exports ) {
+    if ( export_->kind == ExternalKind::Memory ) {
+      if ( export_->name.find( "ro_mem" ) != string::npos ) {
+        exported_ro_.push_back( module->GetMemoryIndex( export_->var ) );
+      }
+      if ( export_->name.find( "rw_mem" ) != string::npos ) {
+        exported_rw_.push_back( module->GetMemoryIndex( export_->var ) );
+      }
+    }
+  }
+
+  for ( Import* import_ : module->imports ) {
+    imported_functions_.push_back( import_->field_name );
+  }
+}
+
+Result WasmInspector::OnMemoryCopyExpr( MemoryCopyExpr* expr )
+{
+  return CheckMemoryAccess( &expr->destmemidx );
+}
+
+Result WasmInspector::OnMemoryFillExpr( MemoryFillExpr* expr )
+{
+  return CheckMemoryAccess( &expr->memidx );
+}
+
+Result WasmInspector::OnMemoryGrowExpr( MemoryGrowExpr* expr )
+{
+  return CheckMemoryAccess( &expr->memidx );
+}
+
+Result WasmInspector::OnMemoryInitExpr( MemoryInitExpr* expr )
+{
+  return CheckMemoryAccess( &expr->memidx );
+}
+
+Result WasmInspector::OnStoreExpr( StoreExpr* expr )
+{
+  return CheckMemoryAccess( &expr->memidx );
+}
+
+void WasmInspector::VisitFunc( Func* func )
+{
+  current_func_ = func;
+  visitor_.VisitFunc( func );
+  current_func_ = nullptr;
+}
+
+void WasmInspector::VisitExport( Export* export_ )
+{
+  if ( export_->name.find( "rw_mem" ) != string::npos ) {
+    result_ = CheckMemoryAccess( &export_->var );
+  }
+  return;
+}
+
+void WasmInspector::VisitGlobal( Global* global )
+{
+  visitor_.VisitExprList( global->init_expr );
+}
+
+void WasmInspector::VisitElemSegment( ElemSegment* segment )
+{
+  visitor_.VisitExprList( segment->offset );
+}
+
+void WasmInspector::VisitDataSegment( DataSegment* segment )
+{
+  visitor_.VisitExprList( segment->offset );
+}
+
+Result WasmInspector::ValidateMemAccess()
+{
+  for ( Func* func : current_module_->funcs )
+    VisitFunc( func );
+  for ( Export* export_ : current_module_->exports )
+    VisitExport( export_ );
+  for ( Global* global : current_module_->globals )
+    VisitGlobal( global );
+  for ( ElemSegment* elem_segment : current_module_->elem_segments )
+    VisitElemSegment( elem_segment );
+  for ( DataSegment* data_segment : current_module_->data_segments )
+    VisitDataSegment( data_segment );
+  return result_;
+}
+
+Result WasmInspector::CheckMemoryAccess( Var* memidx )
+{
+  if ( find( exported_ro_.begin(), exported_ro_.end(), current_module_->GetMemoryIndex( *memidx ) )
+       == exported_ro_.end() ) {
+    return Result::Ok;
+  } else {
+    return Result::Error;
+  }
+}
+
+vector<string> WasmInspector::GetImportedFunctions()
+{
+  return imported_functions_;
+}
+
+std::vector<uint32_t> WasmInspector::GetExportedRO()
+{
+  std::vector<uint32_t> result;
+  for ( Export* export_ : current_module_->exports ) {
+    if ( export_->kind == ExternalKind::Memory ) {
+      if ( export_->name.find( "ro_mem" ) != string::npos ) {
+        result.push_back( (uint32_t)atoi( export_->name.substr( 7 ).data() ) );
+      }
+    }
+  }
+  return result;
+}
+
+std::vector<uint32_t> WasmInspector::GetExportedRW()
+{
+  std::vector<uint32_t> result;
+  for ( Export* export_ : current_module_->exports ) {
+    if ( export_->kind == ExternalKind::Memory ) {
+      if ( export_->name.find( "rw_mem" ) != string::npos ) {
+        result.push_back( (uint32_t)atoi( export_->name.substr( 7 ).data() ) );
+      }
+    }
+  }
+  return result;
+}
+
+} // namespace wasminspector
---
+++ src/compiler/wasminspector.hh
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "src/cast.h"
+#include "src/common.h"
+#include "src/error.h"
+#include "src/expr-visitor.h"
+#include "src/ir.h"
+#include "src/wast-lexer.h"
+
+namespace wasminspector {
+
+class WasmInspector : public wabt::ExprVisitor::DelegateNop
+{
+public:
+  WasmInspector( wabt::Module* module, wabt::Errors* errors );
+  WasmInspector( const WasmInspector& ) = default;
+  WasmInspector& operator=( const WasmInspector& ) = default;
+
+  wabt::Result ValidateMemAccess();
+  std::vector<std::string> GetImportedFunctions();
+  std::vector<uint32_t> GetExportedRO();
+  std::vector<uint32_t> GetExportedRW();
+
+  // Implementation of ExprVisitor::DelegateNop.
+  wabt::Result OnMemoryCopyExpr( wabt::MemoryCopyExpr* ) override;
+  wabt::Result OnMemoryFillExpr( wabt::MemoryFillExpr* ) override;
+  wabt::Result OnMemoryGrowExpr( wabt::MemoryGrowExpr* ) override;
+  wabt::Result OnMemoryInitExpr( wabt::MemoryInitExpr* ) override;
+  wabt::Result OnStoreExpr( wabt::StoreExpr* ) override;
+
+private:
+  void VisitFunc( wabt::Func* func );
+  void VisitExport( wabt::Export* export_ );
+  void VisitGlobal( wabt::Global* global );
+  void VisitElemSegment( wabt::ElemSegment* segment );
+  void VisitDataSegment( wabt::DataSegment* segment );
+  void VisitScriptModule( wabt::ScriptModule* script_module );
+  void VisitCommand( wabt::Command* command );
+  wabt::Result CheckMemoryAccess( wabt::Var* memidx );
+
+  wabt::Errors* errors_ = nullptr;
+  wabt::Module* current_module_ = nullptr;
+  wabt::Func* current_func_ = nullptr;
+  wabt::ExprVisitor visitor_;
+  wabt::Result result_ = wabt::Result::Ok;
+
+  std::vector<std::string> imported_functions_;
+  std::vector<wabt::Index> exported_ro_;
+  std::vector<wabt::Index> exported_rw_;
+};
+
+} // namespace wasminspector
