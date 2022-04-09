6652f0638c798397d9b7a861d1cd30cd313bf68f
Keith Winstein <keithw@cs.stanford.edu>
2022-04-08 22:16:00 -0700

    Test with sanitizers

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -1,3 +1,10 @@
 enable_testing()
 
-add_test(NAME t_addblob WORKING_DIRECTORY COMMAND ${PROJECT_SOURCE_DIR}/tests/add-test)
+add_custom_target (check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -R "^t_"
+                         COMMENT "Testing...")
+
+add_test(NAME t_addblob WORKING_DIRECTORY
+  COMMAND ${PROJECT_SOURCE_DIR}/tests/add-test
+  ${CMAKE_BINARY_DIR}/src/tester/add_tester
+  ${CMAKE_BINARY_DIR}/src/wasm-examples/addblob/addblob.wasm
+)
