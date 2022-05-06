d616c9f23fe29b1e305d29914dca042fdfb7353e
Keith Winstein <keithw@cs.stanford.edu>
2022-05-06 15:53:59 -0700

    Build trivial-demo with clang wasm32 target

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -1,7 +1,6 @@
-set (CMAKE_C_STANDARD 17)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set (CMAKE_BASE_C_FLAGS "${CMAKE_C_FLAGS}")
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -Weverything -Werror")
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -Weverything -Werror -std=c17 -target wasm32")
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET "wasm32")
--- src/trivial-demo.c
+++ src/trivial-demo.c
@@ -1,6 +1,8 @@
-void fixpoint_exit( int x ) __attribute((import_name("fixpoint_exit"), import_module("fixpoint-api")));
+void fixpoint_exit( int x ) __attribute( ( import_name( "fixpoint_exit" ), import_module( "fixpoint-api" ) ) );
 
-void proc_exit( int x ) __attribute((export_name("proc_exit")))
+void proc_exit( int x ) __attribute( ( export_name( "proc_exit" ) ) );
+
+void proc_exit( int x )
 {
   fixpoint_exit( x );
 }
