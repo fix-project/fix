cf8cf238e4542c2018f8d231185b2e8a5eb94046
Angela Montemayor <amontema@cs.stanford.edu>
2022-05-06 16:03:30 -0700

    test commit

--- src/trivial-demo.c
+++ src/trivial-demo.c
@@ -1,8 +1,8 @@
-void fixpoint_exit( int x ) __attribute( ( import_name( "fixpoint_exit" ), import_module( "fixpoint-api" ) ) );
+void fixpoint_exit( int a ) __attribute( ( import_name( "fixpoint_exit" ), import_module( "fixpoint-api" ) ) );
 
-void proc_exit( int x ) __attribute( ( export_name( "proc_exit" ) ) );
+void proc_exit( int a ) __attribute( ( export_name( "proc_exit" ) ) );
 
-void proc_exit( int x )
+void proc_exit( int a )
 {
-  fixpoint_exit( x );
+  fixpoint_exit( a );
 }
