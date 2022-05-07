8101b6cb039da8036a3eaa72550e1610f4b1538c
Keith Winstein <keithw@cs.stanford.edu>
2022-05-07 00:55:52 -0700

    Move wasm-examples from src to testing

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -4,13 +4,13 @@ add_custom_target (check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -R "
                          COMMENT "Testing...")
 
 add_test(NAME t_addblob WORKING_DIRECTORY
-  COMMAND ${PROJECT_SOURCE_DIR}/tests/add-test
+  COMMAND ${PROJECT_SOURCE_DIR}/testing/add-test
   ${CMAKE_BINARY_DIR}/src/tester/add_tester
-  ${CMAKE_BINARY_DIR}/src/wasm-examples/addblob/addblob.wasm
+  ${CMAKE_BINARY_DIR}/testing/wasm-examples/addblob/addblob.wasm
 )
 
 add_test(NAME t_add_simple WORKING_DIRECTORY
-  COMMAND ${PROJECT_SOURCE_DIR}/tests/add-test
+  COMMAND ${PROJECT_SOURCE_DIR}/testing/add-test
   ${CMAKE_BINARY_DIR}/src/tester/add_tester
-  ${CMAKE_BINARY_DIR}/src/wasm-examples/addblob/add-simple.wasm
+  ${CMAKE_BINARY_DIR}/testing/wasm-examples/addblob/add-simple.wasm
 )
