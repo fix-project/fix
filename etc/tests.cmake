dc45938393f58da60600885091e83dd2a30eac3d
Keith Winstein <keithw@cs.stanford.edu>
2022-05-07 00:49:44 -0700

    Add add-simple.wat (commented/simplified add function)

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -8,3 +8,9 @@ add_test(NAME t_addblob WORKING_DIRECTORY
   ${CMAKE_BINARY_DIR}/src/tester/add_tester
   ${CMAKE_BINARY_DIR}/src/wasm-examples/addblob/addblob.wasm
 )
+
+add_test(NAME t_add_simple WORKING_DIRECTORY
+  COMMAND ${PROJECT_SOURCE_DIR}/tests/add-test
+  ${CMAKE_BINARY_DIR}/src/tester/add_tester
+  ${CMAKE_BINARY_DIR}/src/wasm-examples/addblob/add-simple.wasm
+)
