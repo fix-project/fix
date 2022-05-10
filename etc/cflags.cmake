94193cd631b8a8f5d0bab24e25b98046c8a271e8
Yuhan Deng <yhdeng@stanford.edu>
2022-05-09 17:09:12 -0700

    Add reference type flag

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -1,6 +1,6 @@
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set (CMAKE_BASE_C_FLAGS "${CMAKE_C_FLAGS}")
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -Weverything -Werror -std=c17 -Os -target wasm32")
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -Weverything -Werror -std=c17 -Os -target wasm32 -mreference-types")
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET "wasm32")
