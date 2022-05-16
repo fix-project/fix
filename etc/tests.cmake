ad0329f0a02b59271b303a5349e4e87fc8efdb9d
Angela Montemayor <amontema@cs.stanford.edu>
2022-05-16 15:43:44 -0700

    add return3 test

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -2,3 +2,10 @@ enable_testing()
 
 add_custom_target (check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -R "^t_"
                          COMMENT "Testing...")
+
+add_test(NAME t_return3 WORKING_DIRECTORY
+  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/testing/return-test
+  ${CMAKE_CURRENT_BINARY_DIR}/_deps/fixpoint-build/src/tester/wasi_tester
+  ${CMAKE_CURRENT_BINARY_DIR}/src/examples/return3/output.wasm
+)
+
