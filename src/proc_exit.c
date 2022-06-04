9980129ef045e0b4a79ba99b6a6fe1e1f8e6314c
Keith Winstein <keithw@cs.stanford.edu>
2022-06-03 22:22:36 -0700

    Implement proc_exit with Wasm exceptions

--- src/proc_exit.c
+++ src/proc_exit.c
@@ -16,28 +16,23 @@ extern void program_memory_copy_rw_1(uint32_t, uint32_t) __attribute( ( import_m
 extern externref create_blob_rw_mem_0(uint32_t) __attribute( ( import_module("fixpoint"), import_name("create_blob_rw_mem_0") ) );
 extern externref create_blob_rw_mem_1(uint32_t) __attribute( ( import_module("fixpoint"), import_name("create_blob_rw_mem_1") ) );
 extern externref create_tree_rw_table_0(uint32_t) __attribute( ( import_module("fixpoint"), import_name("create_tree_rw_table_0") ) );
-extern void designate_output(externref) __attribute( ( import_module("helper"), import_name("designate_output") ) );
-extern uint32_t get_i32( uint32_t ) __attribute( ( import_module("helper"), import_name("get_i32")));
-extern void start( void ) __attribute( ( import_module("sloth"), import_name("_start")));
+extern uint32_t get_i32( uint32_t ) __attribute( ( import_module( "helper" ), import_name( "get_i32" ) ) );
+extern void run_start( void ) __attribute( ( import_module( "helper" ), import_name( "run-start" ) ) );
 extern void set_return( uint32_t, externref ) __attribute( ( import_module("helper"), import_name("set_return")));
-extern void fixpoint_exit(externref) __attribute( ( import_module("fixpoint"), import_name("exit") ) );
+__attribute( ( noreturn ) ) void flatware_exit( void )
+  __attribute( ( import_module( "helper" ), import_name( "exit" ) ) );
 
-
-void proc_exit( uint32_t rval ) __attribute( ( export_name("proc_exit") ) );
+__attribute( ( noreturn ) ) void proc_exit( uint32_t rval ) __attribute( ( export_name( "proc_exit" ) ) );
 uint32_t fd_close ( int fd ) __attribute( ( export_name("fd_close") ) );
 uint32_t fd_fdstat_get ( uint32_t fd, uint32_t res ) __attribute ( ( export_name("fd_fdstat_get") ) );
 uint32_t fd_seek ( uint32_t fd, int64_t offset, uint32_t whence, uint32_t file_size ) __attribute ( ( export_name("fd_seek") ) ); // not used atm
 uint32_t fd_write ( uint32_t fd, uint32_t vec, uint32_t vec_len, uint32_t retptr0 ) __attribute ( ( export_name("fd_write") ) );
-externref flatware_exit( void );
-externref fixpoint_apply( externref encode ) __attribute( ( export_name("_fixpoint_apply")) );
+externref fixpoint_apply( externref encode ) __attribute( ( export_name( "_fixpoint_apply" ) ) );
 
-void proc_exit(uint32_t rval) {
-  externref a;
+__attribute( ( noreturn ) ) void proc_exit( uint32_t rval )
+{
   // copying rval to rw_memories[0];
-  memory_copy_rw_0(&rval, sizeof(rval));
-  // detach memory
-  a = create_blob_rw_mem_0(sizeof(rval));
-  designate_output(a);
+  memory_copy_rw_0( &rval, sizeof( rval ) );
   flatware_exit();
 }
 
@@ -62,20 +57,15 @@ uint32_t fd_write(__attribute__((unused)) uint32_t fd, uint32_t iovs, __attribut
   return size;
 }
 
-externref fixpoint_apply(__attribute__((unused)) externref encode) {
-  uint32_t zero = 0;
-  start();
-  memory_copy_rw_0(&zero, sizeof(zero));
-  set_return(0, create_blob_rw_mem_0(sizeof(zero)));
-  set_return(1, create_blob_rw_mem_1(stdout.offset));
-  return create_tree_rw_table_0(2);
-}
+externref fixpoint_apply( __attribute__( ( unused ) ) externref encode )
+{
+  const uint32_t zero = 0;
+  memory_copy_rw_0( &zero, sizeof( zero ) );
 
-externref flatware_exit() {
-  externref b;
-  b = create_blob_rw_mem_1(stdout.offset);
-  set_return(1, b);
-  fixpoint_exit(create_tree_rw_table_0(2));
-  return b;
-}
+  run_start();
 
+  set_return( 0, create_blob_rw_mem_0( sizeof( uint32_t ) ) );
+  set_return( 1, create_blob_rw_mem_1( stdout.offset ) );
+
+  return create_tree_rw_table_0( 2 );
+}
