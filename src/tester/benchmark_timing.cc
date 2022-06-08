07f09b681832def8487038dfd08be5d362dd2114
Yuhan Deng <yhdeng@stanford.edu>
2022-06-07 19:17:14 -0700

    [tester]: Update CMake for add.o.

--- src/tester/benchmark_timing.cc
+++ src/tester/benchmark_timing.cc
@@ -7,14 +7,23 @@
 
 using namespace std;
 
-void baseline_function();
-void static_add();
-void virtual_add();
-void char_add();
-void virtual_char_add();
-extern int add( int, int );
-
 static int N;
+
+#define ADD_TEST( func )                                                                                           \
+  {                                                                                                                \
+    for ( int i = 0; i < N; i++ ) {                                                                                \
+      func( i );                                                                                                   \
+    }                                                                                                              \
+    global_timer().summary( cout );                                                                                \
+    reset_global_timer();                                                                                          \
+  }
+
+void baseline_function();
+void static_add( int );
+void virtual_add( int );
+void char_add( int );
+void virtual_char_add( int );
+extern int add( int, int );
 
 int main( int argc, char* argv[] )
 {
@@ -23,12 +32,12 @@ int main( int argc, char* argv[] )
   }
 
   N = atoi( argv[1] );
-
   baseline_function();
-  static_add();
-  virtual_add();
-  char_add();
-  virtual_char_add();
+
+  ADD_TEST( static_add );
+  ADD_TEST( virtual_add );
+  ADD_TEST( char_add );
+  ADD_TEST( virtual_char_add );
   return 0;
 }
 
@@ -41,59 +50,39 @@ void baseline_function()
   reset_global_timer();
 }
 
-void static_add()
+void static_add( int i )
 {
-  for ( int i = 0; i < N; i++ ) {
+  GlobalScopeTimer<Timer::Category::Execution> record_timer;
+  add( i, 42 );
+}
+
+void virtual_add( int i )
+{
+  Base* ptr = new Derived( i, 42 );
+  {
     GlobalScopeTimer<Timer::Category::Execution> record_timer;
-    add( i, 42 );
+    ptr->add( i, 42 );
   }
-
-  global_timer().summary( cout );
-  reset_global_timer();
+  delete ptr;
 }
 
-void virtual_add()
+void char_add( int i )
 {
-  for ( int i = 0; i < N; i++ ) {
-    Base* ptr = new Derived( i, 42 );
-    {
-      GlobalScopeTimer<Timer::Category::Execution> record_timer;
-      ptr->add( i, 42 );
-    }
-    delete ptr;
+  uint32_t x = i;
+  uint32_t y = 42;
+  {
+    GlobalScopeTimer<Timer::Category::Execution> record_timer;
+    add( (char*)&x, (char*)&y );
   }
-
-  global_timer().summary( cout );
-  reset_global_timer();
 }
 
-void char_add()
+void virtual_char_add( int i )
 {
-  for ( int i = 0; i < N; i++ ) {
-    uint32_t x = i;
-    uint32_t y = 42;
-    {
-      GlobalScopeTimer<Timer::Category::Execution> record_timer;
-      add( (char*)&x, (char*)&y );
-    }
+  uint32_t x = i;
+  uint32_t y = 42;
+  Base* ptr = new Derived( i, 42 );
+  {
+    GlobalScopeTimer<Timer::Category::Execution> record_timer;
+    ptr->add_char( (char*)&x, (char*)&y );
   }
-
-  global_timer().summary( cout );
-  reset_global_timer();
-}
-
-void virtual_char_add()
-{
-  for ( int i = 0; i < N; i++ ) {
-    uint32_t x = i;
-    uint32_t y = 42;
-    Base* ptr = new Derived( i, 42 );
-    {
-      GlobalScopeTimer<Timer::Category::Execution> record_timer;
-      ptr->add_char( (char*)&x, (char*)&y );
-    }
-  }
-
-  global_timer().summary( cout );
-  reset_global_timer();
 }
