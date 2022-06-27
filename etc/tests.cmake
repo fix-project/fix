7c4f5aef59ab8e6fb03c336197d926d816ef4487
Keith Winstein <keithw@cs.stanford.edu>
2022-06-27 15:35:08 -0700

    etc/tests.cmake: Add (commented out) flatware tests

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -14,3 +14,21 @@ add_test(NAME t_add_simple WORKING_DIRECTORY
   ${CMAKE_CURRENT_BINARY_DIR}/src/tester/add_tester
   ${CMAKE_CURRENT_BINARY_DIR}/testing/wasm-examples/add-simple.wasm
 )
+
+#add_test(NAME t_return3 WORKING_DIRECTORY
+#  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/testing/return-test
+#  ${CMAKE_CURRENT_BINARY_DIR}/_deps/fixpoint-build/src/tester/wasi_tester
+#  ${CMAKE_CURRENT_BINARY_DIR}/src/examples/return3/output.wasm
+#)
+#
+#add_test(NAME t_hello_world WORKING_DIRECTORY
+#  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/testing/hello-world-test
+#  ${CMAKE_CURRENT_BINARY_DIR}/_deps/fixpoint-build/src/tester/wasi_tester
+#  ${CMAKE_CURRENT_BINARY_DIR}/src/examples/helloworld/output.wasm
+#)
+#
+#add_test(NAME t_add_args WORKING_DIRECTORY
+#  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/testing/add-test
+#  ${CMAKE_CURRENT_BINARY_DIR}/_deps/fixpoint-build/src/tester/wasi_tester
+#  ${CMAKE_CURRENT_BINARY_DIR}/src/examples/add/output.wasm
+#)
