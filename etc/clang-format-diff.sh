62f07daa0295c9cf37e7cb89ad8f73ab757cd8ad
Yuhan Deng <yhdeng@stanford.edu>
2022-11-04 15:29:37 -0700

    Add lint workflow

---
+++ etc/clang-format-diff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+# origin/main isn't fetched by default on GitHub runners
+git fetch --all
+
+FORMAT_MSG=$(git clang-format origin/master -q --diff -- src/)
+if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
+then
+  echo "Please run git clang-format before committing, or apply this diff:"
+  echo
+  # Run git clang-format again, this time without capturing stdout.  This way
+  # clang-format format the message nicely and add color.
+  git clang-format origin/main -q --diff -- src/
+  exit 1
+fi
