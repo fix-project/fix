9e34f8a84efa814d31d9314228f9b29e4b632679
Neha Kunjal <nkunjal@stanford.edu>
2023-01-24 14:54:55 -0800

    Adding open deep/open test to run automatically on push

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -1,7 +1,24 @@
 enable_testing()
 
+file(
+  MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/.fix
+)
+
 add_custom_target (fixpoint-check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -R "^t_"
-                         COMMENT "Testing...")
+                         COMMENT "Testing..."
+                         DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/file.txt)
+
+add_custom_command (OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/file.txt
+  COMMAND ${CMAKE_CURRENT_BINARY_DIR}/src/tester/serialization_test_deep | cut -c28- | head -c -1 > ${CMAKE_CURRENT_BINARY_DIR}/file.txt
+)
+
+add_test(NAME t_deep_open WORKING_DIRECTORY
+  COMMAND bash -c "${CMAKE_CURRENT_BINARY_DIR}/src/tester/wasi_tester ${CMAKE_CURRENT_BINARY_DIR}/flatware-prefix/src/flatware-build/examples/open/open-deep-fixpoint.wasm -h `cat ${CMAKE_CURRENT_BINARY_DIR}/file.txt`"
+)
+
+add_test(NAME t_open WORKING_DIRECTORY
+  COMMAND bash -c "${CMAKE_CURRENT_BINARY_DIR}/src/tester/wasi_tester ${CMAKE_CURRENT_BINARY_DIR}/flatware-prefix/src/flatware-build/examples/open/open-fixpoint.wasm -h `cat ${CMAKE_CURRENT_BINARY_DIR}/file.txt`"
+)
 
 add_test(NAME t_addblob WORKING_DIRECTORY
   COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/testing/add-test
@@ -39,7 +56,6 @@ add_test(NAME t_add_args WORKING_DIRECTORY
   ${CMAKE_CURRENT_BINARY_DIR}/src/tester/wasi_tester
   ${CMAKE_CURRENT_BINARY_DIR}/flatware-prefix/src/flatware-build/examples/add/add-fixpoint.wasm
 )
-
 # XXX we don't catch the trap yet
 #add_test(NAME t_trap WORKING_DIRECTORY
 #  COMMAND ${CMAKE_CURRENT_BINARY_DIR}/src/tester/stateless-tester
