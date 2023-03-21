dab16ffe1eab7656027bd6357f06e90b2cf5cf9f
Katie Gioioso <kgioioso15@gmail.com>
2023-03-21 14:37:10 -0700

    cleanup cmake

--- applications/util/cmake_helper_functions.cmake
+++ applications/util/cmake_helper_functions.cmake
@@ -1,10 +1,10 @@
 # contains functions to create a fixpoint-compatible wasm module out of a c++ object.
 
-# link_libc links cc_obj_file with the libc and generates linked_with_libc.wasm. Depends on cc_obj_rule.
+# link_libc links cc_obj_file with the libc and generates cc_wasm_name. Depends on cc_obj_rule.
 # Note: you also need to link with wasi_snapshot_preview1.wasm via wasmlink (see "link_wasi_stubs" function)
-function(link_libc cc_obj_file cc_obj_rule) 
+function(link_libc cc_obj_file cc_obj_rule cc_wasm_name) 
   add_custom_command(
-    OUTPUT  "linked_with_libc.wasm"
+    OUTPUT ${cc_wasm_name}
     DEPENDS ${cc_obj_rule}
             ${cc_obj_file}
     COMMAND $ENV{HOME}/wasm-toolchain/sysroot/bin/wasm-ld
@@ -13,40 +13,73 @@ function(link_libc cc_obj_file cc_obj_rule)
             $ENV{HOME}/wasm-toolchain/sysroot/lib/wasm32-wasi/libc++abi.a
             $ENV{HOME}/wasm-toolchain/sysroot/lib/wasm32-wasi/libc++.a
             $ENV{HOME}/wasm-toolchain/sysroot/lib/clang/16.0.0/lib/wasi/libclang_rt.builtins-wasm32.a
-            -o linked_with_libc.wasm
+            -o ${cc_wasm_name}
             --no-entry
             --allow-undefined
   )
 endfunction()
 
-# link_storage links linked_with_libc.wasm with wasm module "storage_file_dir/storage_file_name".
-# outputs linked_with_storage.wasm. The name of the rule that generates storage_file_name must be storage_file_name.
-function(link_storage storage_file_dir storage_file_name)
-    set_source_files_properties( ${storage_file_dir}/${storage_file_name} PROPERTIES GENERATED TRUE )
+# link_wasm links cc_wasm_name with wasm module "wasm_file_dir/wasm_file_name".
+# outputs linked_with_wasm.wasm. The name of the rule that generates wasm_file_name 
+# must be wasm_file_name and cc_wasm_name must be in the current directory.
+function(link_wasm wasm_file_dir wasm_file_name cc_wasm_name)
+    set_source_files_properties( ${wasm_file_dir}/${wasm_file_name} PROPERTIES GENERATED TRUE )
 
-    # copy because wasmlink requires modules to be in same directory
-    add_custom_command(
-        OUTPUT ${storage_file_name}
-        DEPENDS ${storage_file_dir}/${storage_file_name}
-        COMMAND cp ${storage_file_dir}/${storage_file_name} .)
+    if (NOT (${wasm_file_dir} STREQUAL ".")) 
+        # copy because wasmlink requires modules to be in same directory
+        add_custom_command(
+            OUTPUT ${wasm_file_name}
+            DEPENDS ${wasm_file_dir}/${wasm_file_name}
+            COMMAND cp ${wasm_file_dir}/${wasm_file_name} .)
+    endif()
 
     add_custom_command(
-        OUTPUT "linked_with_storage.wasm"
+        OUTPUT "linked_with_wasm_0.wasm"
         DEPENDS ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
-        linked_with_libc.wasm
-        ${storage_file_name}
+        ${cc_wasm_name}
+        ${wasm_file_name}
         COMMAND ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
         --enable-multi-memory
         --enable-exceptions
-        linked_with_libc.wasm
-        ${storage_file_name}
-        -o "linked_with_storage.wasm"
+        ${cc_wasm_name}
+        ${wasm_file_name}
+        -o "linked_with_wasm_0.wasm"
     )
 endfunction()
 
-# link_storage links linked_with_storage.wasm with wasm module "wasi_stubs_file_dir/wasi_stubs_file_name".
+# link_another_wasm links linked_with_wasm_{i-1}.wasm with wasm module "wasm_file_name".
+# outputs linked_with_wasm_i.wasm. The name of the rule that generates wasm_file_name must be wasm_file_name.
+function(link_another_wasm wasm_file_dir wasm_file_name wasm_index)
+    set_source_files_properties( ${wasm_file_dir}/${wasm_file_name} PROPERTIES GENERATED TRUE )
+    set(previous_wasm_index ${wasm_index})
+    MATH(EXPR previous_wasm_index "${previous_wasm_index}-1")
+    
+    if (NOT (${wasm_file_dir} STREQUAL ".")) 
+        # copy because wasmlink requires modules to be in same directory
+        add_custom_command(
+            OUTPUT ${wasm_file_name}
+            DEPENDS ${wasm_file_dir}/${wasm_file_name}
+            COMMAND cp ${wasm_file_dir}/${wasm_file_name} .)
+    endif()
+
+    add_custom_command(
+        OUTPUT "linked_with_wasm_${wasm_index}.wasm"
+        DEPENDS ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
+        linked_with_wasm_${previous_wasm_index}.wasm
+        ${wasm_file_name}
+        COMMAND ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
+        --enable-multi-memory
+        --enable-exceptions
+        "linked_with_wasm_${previous_wasm_index}.wasm"
+        ${wasm_file_name}
+        -o "linked_with_wasm_${wasm_index}.wasm"
+    )
+endfunction()
+
+# link_wasm links linked_with_wasm_{num_wasms_linked - 1}.wasm with wasm module
+# "wasi_stubs_file_dir/wasi_stubs_file_name".
 # outputs output_file_name. The name of the rule that generates wasi_stubs_file_name must be wasi_stubs_file_name.
-function(link_wasi_stubs wasi_stubs_file_dir wasi_stubs_file_name output_file_name) 
+function(link_wasi_stubs wasi_stubs_file_dir wasi_stubs_file_name output_file_name num_wasms_linked) 
     set_source_files_properties( ${wasi_stubs_file_dir}/${wasi_stubs_file_name} PROPERTIES GENERATED TRUE )
 
     # copy because wasmlink requires modules to be in same directory
@@ -55,15 +88,18 @@ function(link_wasi_stubs wasi_stubs_file_dir wasi_stubs_file_name output_file_na
         DEPENDS ${wasi_stubs_file_dir}/${wasi_stubs_file_name}
         COMMAND cp ${wasi_stubs_file_dir}/${wasi_stubs_file_name} .)
 
+    set(wasm_index ${num_wasms_linked})
+    MATH(EXPR wasm_index "${wasm_index}-1")
+
     add_custom_command(
         OUTPUT ${output_file_name}
         DEPENDS ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
-        linked_with_storage.wasm
+        linked_with_wasm_${wasm_index}.wasm
         ${wasi_stubs_file_name}
         COMMAND ${CMAKE_BINARY_DIR}/_deps/wasm-tools-build/src/module-combiner/wasmlink
         --enable-multi-memory
         --enable-exceptions
-        linked_with_storage.wasm
+        linked_with_wasm_${wasm_index}.wasm
         ${wasi_stubs_file_name}
         -o ${output_file_name}
     )
