3ce7c0a77c48ba1a91195d5040f8d8d29a108491
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2023-06-23 13:45:41 -0700

    Extract stateless-tester input parsing

---
+++ src/tester/tester-utils.hh
@@ -0,0 +1,21 @@
+#pragma once
+#include <concepts>
+#include <string_view>
+#include <vector>
+
+#include "mmap.hh"
+#include "runtimestorage.hh"
+
+struct pretty_print
+{
+  Handle name;
+  unsigned int level;
+  pretty_print( Handle name, unsigned int level = 0 )
+    : name( name )
+    , level( level )
+  {}
+};
+
+std::ostream& operator<<( std::ostream& stream, const pretty_print& pp );
+
+Handle parse_args( span_view<char*>& args, std::vector<ReadOnlyFile>& open_files );
