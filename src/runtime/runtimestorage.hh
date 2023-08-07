42dfa19d8fc24d4186dda5259f8b499270f3ef1f
tweoss <fchua@puffer5.stanford.edu>
2023-08-07 14:19:33 -0700

    fix open_file lifetime in http-tester

--- src/runtime/runtimestorage.hh
+++ src/runtime/runtimestorage.hh
@@ -166,8 +166,9 @@ public:
 
   // Get dependees. A list of tasks that must complete before this task may be unblocked
   std::vector<Task> get_dependees( Task depender ) { return fix_cache_.get_dependees( depender ); }
-  // Get dependees
-  std::optional<Handle> get_status( Task task ) { return fix_cache_.get( task ); };
+  // Get status. Returns none if the entry does not exist, value(none) if the entry exists
+  // but is not yet evaluated, and value(value(handle)) if the handle exists in cache.
+  std::optional<std::optional<Handle>> get_status( Task task ) { return fix_cache_.get( task ); };
 
   // get entries in local storage for debugging purposes
   Object& local_storage_at( size_t index ) { return local_storage_.at( index ); }
--- src/storage/fixcache.hh
+++ src/storage/fixcache.hh
@@ -163,13 +163,15 @@ public:
    * Gets the cached result of a Task.
    *
    * @param task  The Task to lookup.
-   * @return  The Handle of the result if it was cached, or std::nullopt if it was not.
+   * @return      If a corresponding entry does not exist in the cache, then std::nullopt is returned. value(std::nullopt) 
+   *              is returned if the entry in the cache exists but the result is not cached yet. If the result is 
+   *              cached, then value(value(Handle)) of the result is returned.
    */
-  std::optional<Handle> get( Task task )
+  std::optional<std::optional<Handle>> get( Task task )
   {
     std::shared_lock lock( fixcache_mutex_ );
     if ( fixcache_.contains( task ) ) {
-      return fixcache_.at( task );
+      return { fixcache_.at( task ) };
     } else {
       return {};
     }
