a5a71422fd33423b4aaebefc32a80df4c0e646aa
Matthew Vilaysack <42940122+matthewvilaysack@users.noreply.github.com>
2023-10-04 13:26:46 -0700

    Switch SHA256 implementation to PicoSHA2 (#93)
    - add PicoSHA2 as submodule
    - upgrade actions/checkout to v4 consistently
    - clone submodules on github actions
    - eliminate dependency on libcryptopp

--- src/util/sha256.cc
+++ src/util/sha256.cc
@@ -1,34 +1,14 @@
-#include <cryptopp/filters.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/sha.h>
-
 #include "sha256.hh"
+#pragma GCC diagnostic ignored "-Weffc++"
+#include "PicoSHA2/picosha2.h"
 #include "timer.hh"
 
 using namespace std;
-namespace CryptoPP {
-using byte = unsigned char;
-}
 
 string sha256::encode( string_view input )
 {
   GlobalScopeTimer<Timer::Category::Hash> record_timer;
-  CryptoPP::SHA256 hash;
-
-  CryptoPP::byte digest[CryptoPP::SHA256::DIGESTSIZE];
-  hash.CalculateDigest( digest, (const CryptoPP::byte*)input.data(), input.size() );
-
-  return string( (char*)digest, CryptoPP::SHA256::DIGESTSIZE );
-}
-
-bool sha256::verify( const string& ret, const string& input )
-{
-  GlobalScopeTimer<Timer::Category::Hash> record_timer;
-  CryptoPP::SHA256 hash;
-  bool verified;
-
-  hash.Update( reinterpret_cast<const unsigned char*>( input.data() ), input.size() );
-  verified = hash.Verify( reinterpret_cast<const unsigned char*>( ret.data() ) );
-
-  return verified;
+  string digest( picosha2::k_digest_size, 0 );
+  picosha2::hash256( input, digest );
+  return digest;
 }
