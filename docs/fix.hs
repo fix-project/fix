f6d2bd1968488babd5ada6a754c54e351e8fdba7
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2023-11-07 12:29:28 -0800

    Remove unnecessary Value intermediary

--- docs/fix.hs
+++ docs/fix.hs
@@ -3,7 +3,7 @@ import qualified Data.Set as Set
 
 -- | Describes, essentially, a file on disk/in RAM.  A Blob stores a sequence
 -- of bytes, while a Tree stores a sequence of Handles.  Note that this usage
--- of "Blob" and "Tree" is slightly different than the Values below.
+-- of "Blob" and "Tree" is slightly different than the Names below.
 data Data = BlobData StoredBlob | TreeData StoredTree deriving (Eq, Ord)
 type StoredBlob  = [Char]
 type StoredTree = [Handle]
@@ -26,24 +26,21 @@ instance DataId TreeId StoredTree where
   load = undefined
   create = undefined
 
--- | A Value is a piece of literal data; it may be a Blob (arbitrary bytes), a
--- Tree (a sequence of Handles), or a Tag (a claim that a certain procedure has
--- assigned a piece of data a specific "type").
-data Value = Blob BlobId | Tree TreeId | Tag TreeId deriving (Eq, Ord)
+-- | A Name is a way to identify a Blob (a sequence of bytes), a Tree (a
+-- sequence of Handles), a Tag (a "signature" produced by a computation), or a
+-- Thunk (a computation)
+data Name = Blob BlobId | Tree TreeId | Tag TreeId | Thunk TreeId deriving (Eq, Ord)
 
--- | A Name is a way to identify either a Value or a Thunk; Thunks represent
--- computations.  When evaluated, a Thunk turns into a Value.
-data Name = Value Value | Thunk TreeId deriving (Eq, Ord)
-
--- | Sometimes a computation doesn't need access to a full Value, but only an
--- opaque reference to it that it can pass along to another computation.  A
--- Handle represents a certain access level for a certain name.
+-- | Sometimes a computation doesn't need access to the full data of a Name,
+-- but only an opaque reference to it that it can pass along to another
+-- computation.  A Handle represents a certain access level for a certain Name.
 data Handle = Handle (Name, Accessibility) deriving (Eq, Ord)
+
 -- | Strict accessibility means a running computation can see the full contents
--- of a Value, essentially calling `load` as described above.  Shallow
+-- of a Name, essentially calling `load` as described above.  Shallow
 -- accessibility means a running computation can see certain information about
 -- a Handle, including getting lazy versions of the first layer of Trees.  Lazy
--- accessibility means a running computation cannot inspect a Value at all, but
+-- accessibility means a running computation cannot inspect a Name at all, but
 -- only include it in its result.
 data Accessibility = Lazy | Shallow | Strict deriving (Eq, Ord)
 
@@ -56,9 +53,9 @@ data NameData = JustName Name | NameAndSize (Name, Int) | NameAndData (Name, Dat
 -- | Look up the Data corresponding to a Name.
 deref :: Name -> Data
 deref name = case name of
-  Value (Blob id) -> BlobData $ load id
-  Value (Tree id) -> TreeData $ load id
-  Value (Tag id) -> TreeData $ load id
+  Blob id -> BlobData $ load id
+  Tree id -> TreeData $ load id
+  Tag id -> TreeData $ load id
   Thunk id -> TreeData $ load id
 
 -- | Get the length of some Data.
@@ -70,31 +67,31 @@ size (TreeData t) = length t
 dat :: Handle -> NameData
 dat handle = case handle of
   Handle (n, Lazy) -> JustName n
-  Handle (Value v, Shallow) -> NameAndSize (Value v, size $ deref $ Value v)
-  Handle (Value v, Strict) -> NameAndData (Value v, deref $ Value v)
+  Handle (n, Shallow) -> NameAndSize (n, size $ deref n)
+  Handle (n, Strict) -> NameAndData (n, deref n)
 
 -- | A core Fix operation; apply runs a computation represented by a Tree in a
 -- specified (ENCODE) format.
 apply :: Handle -> Handle
-apply (Handle (Value (Tree _), Strict)) = undefined
+apply (Handle (Tree _, Strict)) = undefined
 
 -- | A core Fix operation; eval applies any strict or shallow Thunks within an
 -- Object.
 eval :: Handle -> Handle
 eval handle = case handle of
   Handle (_, Lazy) -> handle
-  Handle (Value (Blob b), _) -> handle
-  Handle (Value (Tree t), Shallow) -> handle
-  Handle (Value (Tag t), Shallow) -> handle
+  Handle (Blob b, _) -> handle
+  Handle (Tree t, Shallow) -> handle
+  Handle (Tag t, Shallow) -> handle
   Handle (Thunk t, Shallow) -> eval $ shallow $ apply $ eval $ encode t
-  Handle (Value (Tree t), Strict) -> Handle(Value (Tree $ recurse t), Strict)
-  Handle (Value (Tag t), Strict) -> Handle(Value (Tag $ recurse t), Strict)
+  Handle (Tree t, Strict) -> Handle(Tree $ recurse t, Strict)
+  Handle (Tag t, Strict) -> Handle(Tag $ recurse t, Strict)
   Handle (Thunk t, Strict) -> eval $ apply $ eval $ encode t
   where recurse = create . map eval . load
-        encode t = Handle(Value (Tree t), Strict)
+        encode t = Handle(Tree t, Strict)
         shallow x = case x of
-          Handle (Value (Tree t), Strict) -> Handle (Value (Tree t), Shallow)
-          Handle (Value (Tag t), Strict) -> Handle (Value (Tag t), Shallow)
+          Handle (Tree t, Strict) -> Handle (Tree t, Shallow)
+          Handle (Tag t, Strict) -> Handle (Tag t, Shallow)
           h -> h
 
 -- | Calculates the set of Handles which are necessary to begin application of
@@ -104,9 +101,9 @@ attachableSet handle = case handle of
   Handle (_, Lazy) -> Set.singleton handle
   Handle (Thunk t, _) -> Set.insert handle $ recurse t
   Handle (_, Shallow) -> Set.singleton $ handle
-  Handle (Value (Blob _), Strict) -> Set.singleton $ handle
-  Handle (Value (Tree t), Strict) -> Set.insert handle $ recurse t
-  Handle (Value (Tag t), Strict) -> Set.insert handle $ recurse t
+  Handle (Blob _, Strict) -> Set.singleton $ handle
+  Handle (Tree t, Strict) -> Set.insert handle $ recurse t
+  Handle (Tag t, Strict) -> Set.insert handle $ recurse t
   where recurse id = foldr
                       Set.union
                       Set.empty
@@ -119,9 +116,9 @@ attachableData = Set.map dat . attachableSet
 -- object.
 reachableSet :: Name -> Set Name
 reachableSet name = case name of
-  Value (Blob _) -> Set.singleton name
-  Value (Tree t) -> Set.insert name $ recurse t
-  Value (Tag t) -> Set.insert name $ recurse t
+  Blob _ -> Set.singleton name
+  Tree t -> Set.insert name $ recurse t
+  Tag t -> Set.insert name $ recurse t
   Thunk t -> Set.insert name $ recurse t
   where recurse id = foldr
                       Set.union
