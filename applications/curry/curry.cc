ecae5eff420c66b2c00bf03f4d0374f1d7da8fb8
Sebastian Ingino <ingino@stanford.edu>
2023-11-28 21:31:07 -0800

    Add success tag to curry

--- applications/curry/curry.cc
+++ applications/curry/curry.cc
@@ -13,12 +13,26 @@ typedef enum VALUE_TYPE
 } VALUE_TYPE;
 
 /**
+ * @brief Tags a value with a boolean indicating whether the program succeeded.
+ *
+ * @param value       The value to tag.
+ * @param succeeded   Whether the program succeeded.
+ * @return externref  The tagged value.
+ */
+externref tagged_output( externref value, bool succeeded )
+{
+  grow_rw_mem_0_pages( 4 );
+  set_i32_rw_mem_0( 0, succeeded );
+  return create_tag( value, create_blob_rw_mem_0( 4 ) );
+}
+
+/**
  * @brief Runs a curried function
  *
- * @param resource_limits - current resource_limits
- * @param joined_encode - tree containing (in order) initial resource_limits, current program, program to run,
- * number of required arguments, and arguments
- * @return externref - a thunk to the full apply, or resource_limits if the program was aborted
+ * @param resource_limits   The current resource limits.
+ * @param joined_encode     Tree containing (in order) initial resource_limits, current program, program to run,
+ *                          number of required arguments, and arguments.
+ * @return externref        The thunk to the full apply tagged as success, or the curried function tagged as a failure.
  */
 externref run( externref resource_limits, externref joined_encode )
 {
@@ -31,7 +45,7 @@ externref run( externref resource_limits, externref joined_encode )
 
   int32_t old_size = grow_rw_table_0( num_args + 2, resource_limits ); // +2 for resource_limits and program
   if ( old_size == -1 ) {
-    return resource_limits;
+    return tagged_output( joined_encode, false );
   }
 
   set_rw_table_0( 0, curried_resource_limits );
@@ -40,22 +54,24 @@ externref run( externref resource_limits, externref joined_encode )
     set_rw_table_0( i + 2, get_ro_table_0( i + 4 ) );
   }
 
-  return create_thunk( create_tree_rw_table_0( num_args + 2 ) );
+  return tagged_output( create_thunk( create_tree_rw_table_0( num_args + 2 ) ), true );
 }
 
 /**
  * @brief Applies the provided arguments to a curried function
  *
- * @param encode - the program being run, with a sub_encode as the second argument
- * @return externref - the curried function as a tree if a partial apply, a thunk if a full apply,
- * or resource_limits if the program was aborted
+ * @param encode      The program being run, with a sub_encode as the second argument.
+ * @return externref  The curried function as a tree if a partial apply tagged as successful if the arguments are applied, 
+ *                    or a thunk if a full apply tagged successful.
  */
 externref apply( externref encode )
 {
   attach_tree_ro_table_0( encode );
 
   externref resource_limits = get_ro_table_0( 0 );
-  externref sub_encode = get_ro_table_0( 1 );
+
+  attach_tree_ro_table_1( get_ro_table_0( 1 ) );
+  externref sub_encode = get_ro_table_1( 0 );
 
   int32_t tree_length = get_length( sub_encode ) + get_length( encode ) - 2;
 
@@ -65,7 +81,7 @@ externref apply( externref encode )
 
   int32_t old_size = grow_rw_table_0( tree_length, resource_limits );
   if ( old_size == -1 ) {
-    return resource_limits;
+    return tagged_output( sub_encode, false );
   }
 
   for ( uint32_t i = 0; i < get_length( sub_encode ); i++ ) { // Copy sub_encode
@@ -82,23 +98,24 @@ externref apply( externref encode )
     return run( resource_limits, joined_encode );
   }
 
-  return joined_encode;
+  return tagged_output( joined_encode, true );
 }
 
 /**
  * @brief Converts a function into a curried function, or applies the arguments to a curried function
- * @details Passes the function to curry or the curried function as the 1st argument.
- *  If creating a curried function, passes the number of arguments as the 2nd argument. If applying arguments,
- * passes the arguments as all subsequent arguments.
- * @return externref - a callable curried function, resource_limits if the program was aborted, or a thunk to the full apply if
- * enough arguments are provided
+ * @details Passes the function to curry or the tagged curried function as the 1st argument.
+ *          If creating a curried function, passes the number of arguments as the 2nd argument.
+ *          If applying arguments, passes the arguments as all subsequent arguments.
+ * @return externref  A callable curried function tagged based on success, or a thunk to the
+ *                    full apply tagged successful if enough arguments are provided.
  */
 __attribute__( ( export_name( "_fixpoint_apply" ) ) ) externref _fixpoint_apply( externref encode )
 {
   attach_tree_ro_table_0( encode );
+  attach_tree_ro_table_1( get_ro_table_0( 1 ) ); // Attach tag
 
-  if ( get_value_type( get_ro_table_0( 1 ) ) == VALUE_TYPE::Tag ) { // Initial (unwrapped) call
-    return encode;
+  if ( get_value_type( get_ro_table_1( 0 ) ) == VALUE_TYPE::Blob ) { // Initial (unwrapped) call
+    return tagged_output( encode, true );
   }
 
   return apply( encode );
--- src/tests/test-curry.cc
+++ src/tests/test-curry.cc
@@ -14,7 +14,7 @@ Handle curry( Runtime& rt, Handle program, Handle num_args )
   } ) ) );
 }
 
-Handle apply_args( Runtime& rt, Handle curried, std::initializer_list<Handle> elements )
+Handle apply_args( Runtime& rt, Handle curried, const initializer_list<Handle>& elements )
 {
   for ( auto& e : elements ) {
     curried = rt.eval( thunk( tree( {
@@ -26,19 +26,36 @@ Handle apply_args( Runtime& rt, Handle curried, std::initializer_list<Handle> el
   return curried;
 }
 
+optional<uint32_t> unwrap_tag( Runtime& rt, const Handle& tag )
+{
+  if ( tag.get_content_type() != ContentType::Tag ) {
+    cout << "unwrap_tag: Expected tag, got " << tag.get_content_type() << endl;
+    return {};
+  }
+  auto& tag_state = rt.storage().get_tree( tag )[2];
+  if ( not rt.storage().compare_handles( tag_state, Handle( true ) ) ) {
+    cout << "unwrap_tag: Tag was not successful" << endl;
+    return {};
+  }
+
+  auto& tag_contents = rt.storage().get_tree( tag )[0];
+  uint32_t blob_result = -1;
+  memcpy( &blob_result, rt.storage().get_blob( tag_contents ).data(), sizeof( uint32_t ) );
+  return blob_result;
+}
+
 void test_add_simple( Runtime& rt )
 {
   Handle curried = curry( rt, compile( file( "testing/wasm-examples/add-simple.wasm" ) ), Handle( 2 ) );
 
   Handle curried_result = apply_args( rt, curried, { Handle( 1 ), Handle( 3 ) } );
 
-  uint32_t blob_result = -1;
-  memcpy( &blob_result, rt.storage().get_blob( curried_result ).data(), sizeof( uint32_t ) );
-
-  if ( blob_result == 4 ) {
-    exit( 0 );
+  auto optional_result = unwrap_tag( rt, curried_result );
+  if ( not optional_result ) {
+    printf( "test_add_simple: Result was not successful\n" );
+  } else if ( optional_result.value() != 4 ) {
+    printf( "test_add_simple: Result did not match: %d != 4\n", optional_result.value() );
   }
-  printf( "test_add_simple: Result did not match: %d != 4\n", blob_result );
 }
 
 void test_add_as_encode( Runtime& rt )
@@ -52,13 +69,12 @@ void test_add_as_encode( Runtime& rt )
 
   Handle curried_result = apply_args( rt, curried, { Handle( 1 ), Handle( 3 ) } );
 
-  uint32_t blob_result = -1;
-  memcpy( &blob_result, rt.storage().get_blob( curried_result ).data(), sizeof( uint32_t ) );
-
-  if ( blob_result == 4 ) {
-    exit( 0 );
+  auto optional_result = unwrap_tag( rt, curried_result );
+  if ( not optional_result ) {
+    printf( "test_add_as_encode: Result was not successful\n" );
+  } else if ( optional_result.value() != 4 ) {
+    printf( "test_add_as_encode: Result did not match: %d != 4\n", optional_result.value() );
   }
-  printf( "test_add_as_encode: Result did not match: %d != 4\n", blob_result );
 }
 
 void test_curry_self( Runtime& rt )
@@ -69,14 +85,15 @@ void test_curry_self( Runtime& rt )
   Handle curried_curry_result
     = apply_args( rt, curried, { compile( file( "testing/wasm-examples/add-simple.wasm" ) ), Handle( 2 ) } );
 
-  Handle curried_add_result = apply_args( rt, curried_curry_result, { Handle( 1 ), Handle( 3 ) } );
-  uint32_t result = -1;
-  memcpy( &result, rt.storage().get_blob( curried_add_result ).data(), sizeof( uint32_t ) );
+  Handle unwrapped_curried_curry_result = rt.storage().get_tree( curried_curry_result )[0];
+  Handle curried_add_result = apply_args( rt, unwrapped_curried_curry_result, { Handle( 1 ), Handle( 3 ) } );
 
-  if ( result == 4 ) {
-    exit( 0 );
+  auto optional_result = unwrap_tag( rt, curried_add_result );
+  if ( not optional_result ) {
+    printf( "test_curry_self: Result was not successful\n" );
+  } else if ( optional_result.value() != 4 ) {
+    printf( "test_curry_self: Result did not match: %d != 4\n", optional_result.value() );
   }
-  printf( "test_curry_self: Result did not match: %d != 4\n", result );
 }
 
 void test( void )
