a6496477b5f54931a91e0e400e7dfc3071baecfd
Yuhan Deng <yhdeng@stanford.edu>
2024-01-12 13:01:23 -0800

    Update fixpointapi

--- src/runtime/fixpointapi.hh
+++ src/runtime/fixpointapi.hh
@@ -1,51 +1,80 @@
 #pragma once
 
 #include "wasm-rt.h"
+#include "handle.hh"
 
 namespace fixpoint {
-void attach_tree( __m256i ro_handle, wasm_rt_externref_table_t* target_memory );
+// Traps if handle is not Handle<ObjectTree> or Handle<ValueTree> or Handle<ExpressionTree>
+void attach_tree( u8x32 handle, wasm_rt_externref_table_t* target_memory );
 
-/* Traps if handle is inaccessible, if handle does not refer to a Blob */
-void attach_blob( __m256i ro_handle, wasm_rt_memory_t* target_memory );
+// Traps if handle is not Handle<Blob> (Handle<Named> or Handle<Literal>)
+void attach_blob( u8x32 handle, wasm_rt_memory_t* target_memory );
 
-__m256i create_tree( wasm_rt_externref_table_t* table, size_t size );
+// Return Handle<ObjectTree> or Handle<ValueTree> or Handle<ExpressionTree>
+u8x32 create_tree( wasm_rt_externref_table_t* table, size_t size );
 
-__m256i create_blob( wasm_rt_memory_t* memory, size_t size );
+// Return Handle<Named> or Handle<Literal>
+u8x32 create_blob( wasm_rt_memory_t* memory, size_t size );
 
-__m256i create_tag( __m256i handle, __m256i type );
+// Return a tagged Handle<ObjectTree> or Handle<ValueTree> or Handle<ExpressionTree>
+u8x32 create_tag( u8x32 handle, u8x32 type );
 
-__m256i create_blob_i32( uint32_t content );
+// Return Handle<Blob>
+u8x32 create_blob_i32( uint32_t content );
 
-__m256i create_thunk( __m256i ro_handle );
+// Return Handle<Blob>
+u8x32 create_blob_i64( uint64_t content );
 
-uint32_t get_value_type( __m256i handle );
+// Return Handle<Blob>
+u8x32 create_blob_string( uint32_t idx, uint32_t length, wasm_rt_memory_t* memory );
+
+// Return Handle<Combination>, traps if handle is not Handle<ExpressionTree>
+u8x32 create_combination_thunk( u8x32 handle );
+
+// Return Handle<Identity>, traps if handle is not Handle<Value>
+u8x32 create_identity_thunk( u8x32 handle );
+
+// Return Handle<Identity>, traps if handle is not Handle<Tree> or Handle<TreeStub>
+u8x32 create_select_thunk( u8x32 handle, uint32_t idx );
+
+// Traps if handle is not Handle<Tree> or Handle<TreeStub> or Handle<Blob> or Handle<BlobStub>
+uint32_t get_length( u8x32 handle );
+
+// Return Handle<Strict>, traps if handle is not Handle<Thunk>
+u8x32 create_strict_encode( u8x32 handle );
+
+// Return Handle<Shallow>, traps if handle is not Handle<Thunk>
+u8x32 create_shallow_encode( u8x32 handle );
+
+// Traps if lhs or rhs is not Handle<Object>
+uint32_t is_equal( u8x32 lhs, u8x32 rhs );
+
+// XXX
+uint32_t is_blob( u8x32 handle );
+uint32_t is_tree( u8x32 handle );
+uint32_t is_blob_stub( u8x32 handle );
+uint32_t is_tree_stub( u8x32 handle );
+uint32_t is_value( u8x32 handle );
+uint32_t is_object( u8x32 handle );
+uint32_t is_thunk( u8x32 handle );
+uint32_t is_encode( u8x32 handle );
+uint32_t is_shallow( u8x32 handle );
+uint32_t is_strict( u8x32 handle );
 
 // Unsafe_io prints the contents mem[str_index: index + length]. Traps if
 // index + length - 1 is out of bounds.
-void unsafe_io( int32_t index, int32_t length, wasm_rt_memory_t* mem );
+void unsafe_io( uint32_t index, uint32_t length, wasm_rt_memory_t* memory );
 
-uint32_t equality( __m256i lhs, __m256i rhs );
-
-uint32_t pin( __m256i src, __m256i dst );
-
-// Length in elements (Tree-like) or bytes (Blobs)
-uint32_t get_length( __m256i handle );
-
-// Accessibility (0=strict, 1=shallow, 2=lazy)
-uint32_t get_access( __m256i handle );
-
-// Reduce the accessibility of a Handle
-__m256i lower( __m256i handle );
-
+uint32_t pin( u8x32 src, u8x32 dst );
 }
 
 namespace fixpoint_debug {
 // Attempt to lift the current handle to a stronger accessibility (nondeterministic)
-__m256i try_lift( __m256i handle );
+u8x32 try_lift( u8x32 handle );
 
 // Attempt to get the Encode from a Handle to a Thunk (nondeterministic if we add GC)
-__m256i try_inspect( __m256i handle );
+u8x32 try_inspect( u8x32 handle );
 
 // Attempt to get the evaluated version of a Handle (nondeterministic)
-__m256i try_evaluate( __m256i handle );
+u8x32 try_evaluate( u8x32 handle );
 }
