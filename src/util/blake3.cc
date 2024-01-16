66982766393cd4936ba3e9395311b1ba232f4c0c
Matthew Vilaysack <mvilay02@stanford.edu>
2024-01-16 15:05:18 -0800

    Added back blake3 tests, changed blake3's encode to no longer malloc, uncommented blake3 tests

--- src/util/blake3.cc
+++ src/util/blake3.cc
@@ -5,6 +5,7 @@
 #endif
 #include "blake3.h"
 #include "timer.hh"
+#include <cstring>
 
 using namespace std;
 
@@ -13,11 +14,9 @@ std::string encode( const std::string_view input )
 {
   blake3_hasher hasher;
   blake3_hasher_init( &hasher );
-
   blake3_hasher_update( &hasher, input.data(), input.size() );
-
-  uint8_t hash_buf[BLAKE3_OUT_LEN];
-  blake3_hasher_finalize( &hasher, hash_buf, BLAKE3_OUT_LEN );
-  return std::string( (char*)hash_buf, BLAKE3_OUT_LEN );
+  string digest( BLAKE3_OUT_LEN, '\0' );
+  blake3_hasher_finalize( &hasher, reinterpret_cast<uint8_t*>( digest.data() ), BLAKE3_OUT_LEN );
+  return digest;
 }
 }
