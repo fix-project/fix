d8f87868272409938d2d4b0a3ac17cdcd9c4307e
Yuhan Deng <yhdeng@stanford.edu>
2024-02-01 16:52:04 -0800

    Add implementation of Client and Server

--- src/runtime/runtimes.cc
+++ src/runtime/runtimes.cc
@@ -1,6 +1,8 @@
 #include "runtimes.hh"
+#include "handle.hh"
+#include "object.hh"
 #include "overload.hh"
-#include "storage_exception.hh"
+#include "types.hh"
 
 using namespace std;
 
@@ -20,76 +22,73 @@ shared_ptr<ReadWriteRT> ReadWriteRT::init()
   return runtime;
 }
 
-optional<BlobData> ReadOnlyRT::get( Handle<Named> name )
+template<typename T, typename S>
+optional<T> ReadOnlyRT::get( Handle<S> name )
 {
   if ( executor_->contains( name ) ) {
     return executor_->get( name );
   } else if ( repository_->contains( name ) ) {
-    auto blob = repository_->get( name );
-    executor_->put( name, *blob );
-    return *blob;
+    auto data = repository_->get( name );
+    executor_->put( name, *data );
+    return *data;
   }
 
-  throw HandleNotFound( name );
+  return executor_->get( name );
 }
 
+optional<BlobData> ReadOnlyRT::get( Handle<Named> name )
+{
+  return get<BlobData, Named>( name );
+}
 optional<TreeData> ReadOnlyRT::get( Handle<AnyTree> name )
 {
-  if ( executor_->contains( name ) ) {
-    return executor_->get( name );
-  } else if ( repository_->contains( name ) ) {
-    auto tree = repository_->get( name );
-    executor_->put( name, *tree );
-    return *tree;
-  }
-
-  throw HandleNotFound( handle::fix( name ) );
-};
-
+  return get<TreeData, AnyTree>( name );
+}
 optional<Handle<Object>> ReadOnlyRT::get( Handle<Relation> name )
 {
-  if ( repository_->contains( name ) ) {
-    auto relation = repository_->get( name );
-    repository_->put( name, *relation );
-    return *relation;
-  } else {
-    return executor_->get( name );
-  }
-};
+  return get<Handle<Object>, Relation>( name );
+}
+
+template<typename T, typename S>
+void ReadOnlyRT::put( Handle<S> name, T data )
+{
+  return executor_->put( name, data );
+}
 
 void ReadOnlyRT::put( Handle<Named> name, BlobData data )
 {
-  executor_->put( name, data );
+  return put<BlobData, Named>( name, data );
 }
-
 void ReadOnlyRT::put( Handle<AnyTree> name, TreeData data )
 {
-  executor_->put( name, data );
+  return put<TreeData, AnyTree>( name, data );
 }
-
 void ReadOnlyRT::put( Handle<Relation> name, Handle<Object> data )
 {
-  executor_->put( name, data );
+  return put<Handle<Object>, Relation>( name, data );
+}
+
+template<typename T>
+bool ReadOnlyRT::contains( T name )
+{
+  return executor_->contains( name ) || repository_->contains( name );
 }
 
 bool ReadOnlyRT::contains( Handle<Named> handle )
 {
-  return executor_->contains( handle ) || repository_->contains( handle );
+  return contains<Handle<Named>>( handle );
 }
-
 bool ReadOnlyRT::contains( Handle<AnyTree> handle )
 {
-  return executor_->contains( handle ) || repository_->contains( handle );
+  return contains<Handle<AnyTree>>( handle );
 }
-
 bool ReadOnlyRT::contains( Handle<Relation> handle )
 {
-  return executor_->contains( handle ) || repository_->contains( handle );
+  return contains<Handle<Relation>>( handle );
 }
-
 bool ReadOnlyRT::contains( const std::string_view label )
 {
-  return repository_->contains( label ) || executor_->contains( label );
+  return contains<const std::string_view>( label );
 }
 
 Handle<Fix> ReadOnlyRT::labeled( const std::string_view label )
@@ -97,7 +96,6 @@ Handle<Fix> ReadOnlyRT::labeled( const std::string_view label )
   if ( repository_->contains( label ) ) {
     return repository_->labeled( label );
   }
-
   return executor_->labeled( label );
 }
 
@@ -106,23 +104,206 @@ Handle<Value> ReadOnlyRT::execute( Handle<Relation> x )
   return executor_->execute( x );
 }
 
-template<FixType T>
-void ReadWriteRT::serialize( Handle<T> handle )
-{
-  if constexpr ( Handle<T>::is_fix_sum_type and not Handle<T>::is_fix_wrapper ) {
-    return std::visit( [this]( const auto x ) { return this->serialize( x ); }, handle.get() );
-  } else {
-    if constexpr ( is_same_v<T, Named> or FixTreeType<T> or is_same_v<T, Apply> or is_same_v<T, Eval> ) {
-      repository_->put( handle, executor_->get( handle ).value() );
-    }
-  }
-}
-
 Handle<Value> ReadWriteRT::execute( Handle<Relation> x )
 {
   auto res = executor_->execute( x );
 
-  cout << "Serializing minrepo" << endl;
-  executor_->visit( x, [this]( Handle<Fix> h ) { serialize( h ); } );
+  executor_->visit( x, [this]( Handle<AnyDataType> h ) {
+    h.visit<void>(
+      overload { []( Handle<Literal> ) {},
+                 [&]( auto handle ) { repository_->put( handle, executor_->get( handle ).value() ); } } );
+  } );
   return res;
+}
+
+shared_ptr<Client> Client::init( const Address& address )
+{
+  auto runtime = make_shared<Client>();
+  runtime->repository_.emplace();
+  runtime->network_worker_.emplace( runtime );
+  runtime->network_worker_->connect( address );
+  runtime->remote_ = runtime->network_worker_->get_remote( address );
+  runtime->executor_.emplace( 0, runtime );
+  return runtime;
+}
+
+shared_ptr<Server> Server::init( const Address& address )
+{
+  auto runtime = std::make_shared<Server>();
+  runtime->repository_.emplace();
+  runtime->network_worker_.emplace( runtime );
+  runtime->network_worker_->start_server( address );
+  runtime->executor_.emplace( std::thread::hardware_concurrency(), runtime );
+  return runtime;
+}
+
+Handle<Value> Client::execute( Handle<Relation> x )
+{
+  auto send = [&]( Handle<AnyDataType> h ) {
+    h.visit<void>( overload { []( Handle<Literal> ) {},
+                              []( Handle<Relation> ) {},
+                              [&]( auto x ) { remote_->put( x, executor_->get( x ).value() ); } } );
+  };
+
+  x.visit<void>( overload { [&]( Handle<Apply> x ) { executor_->visit_minrepo( x.unwrap<ObjectTree>(), send ); },
+                            [&]( Handle<Eval> x ) { executor_->visit( x.unwrap<Object>(), send ); } } );
+
+  remote_->get( x );
+  return executor_->execute( x );
+}
+
+template<typename T, typename S>
+optional<T> Client::get( Handle<S> name )
+{
+  if ( executor_->contains( name ) )
+    return executor_->get( name );
+
+  if ( repository_->contains( name ) ) {
+    auto data = repository_->get( name );
+    executor_->put( name, *data );
+    return *data;
+  }
+
+  return remote_->get( name );
+}
+
+optional<BlobData> Client::get( Handle<Named> name )
+{
+  return get<BlobData, Named>( name );
+}
+optional<TreeData> Client::get( Handle<AnyTree> name )
+{
+  return get<TreeData, AnyTree>( name );
+}
+optional<Handle<Object>> Client::get( Handle<Relation> name )
+{
+  return get<Handle<Object>, Relation>( name );
+}
+
+template<typename T, typename S>
+void Client::put( Handle<S> name, T data )
+{
+  executor_->put( name, data );
+}
+
+void Client::put( Handle<Named> name, BlobData data )
+{
+  return put<BlobData, Named>( name, data );
+}
+void Client::put( Handle<AnyTree> name, TreeData data )
+{
+  return put<TreeData, AnyTree>( name, data );
+}
+void Client::put( Handle<Relation> name, Handle<Object> data )
+{
+  return put<Handle<Object>, Relation>( name, data );
+}
+
+template<typename T>
+bool Client::contains( T name )
+{
+  return executor_->contains( name ) || repository_->contains( name ) || remote_->contains( name );
+}
+
+bool Client::contains( Handle<Named> handle )
+{
+  return contains<Handle<Named>>( handle );
+}
+bool Client::contains( Handle<AnyTree> handle )
+{
+  return contains<Handle<AnyTree>>( handle );
+}
+bool Client::contains( Handle<Relation> handle )
+{
+  return contains<Handle<Relation>>( handle );
+}
+bool Client::contains( const std::string_view label )
+{
+  return contains<const std::string_view>( label );
+}
+
+Handle<Fix> Client::labeled( const std::string_view label )
+{
+  if ( repository_->contains( label ) ) {
+    return repository_->labeled( label );
+  }
+  return executor_->labeled( label );
+}
+
+template<typename T, typename S>
+optional<T> Server::get( Handle<S> name )
+{
+  if ( executor_->contains( name ) )
+    return executor_->get( name );
+
+  if ( repository_->contains( name ) )
+    return repository_->get( name );
+
+  return executor_->get( name );
+}
+
+optional<BlobData> Server::get( Handle<Named> name )
+{
+  return get<BlobData, Named>( name );
+}
+optional<TreeData> Server::get( Handle<AnyTree> name )
+{
+  return get<TreeData, AnyTree>( name );
+}
+optional<Handle<Object>> Server::get( Handle<Relation> name )
+{
+  return get<Handle<Object>, Relation>( name );
+}
+
+template<typename T, typename S>
+void Server::put( Handle<S> name, T data )
+{
+  executor_->put( name, data );
+  for ( const auto& connection : network_worker_->connections_.read().get() ) {
+    connection.second->put( name, data );
+  }
+}
+
+void Server::put( Handle<Named> name, BlobData data )
+{
+  return put<BlobData, Named>( name, data );
+}
+void Server::put( Handle<AnyTree> name, TreeData data )
+{
+  return put<TreeData, AnyTree>( name, data );
+}
+void Server::put( Handle<Relation> name, Handle<Object> data )
+{
+  return put<Handle<Object>, Relation>( name, data );
+}
+
+template<typename T>
+bool Server::contains( T name )
+{
+  return executor_->contains( name ) || repository_->contains( name );
+}
+
+bool Server::contains( Handle<Named> handle )
+{
+  return contains<Handle<Named>>( handle );
+}
+bool Server::contains( Handle<AnyTree> handle )
+{
+  return contains<Handle<AnyTree>>( handle );
+}
+bool Server::contains( Handle<Relation> handle )
+{
+  return contains<Handle<Relation>>( handle );
+}
+bool Server::contains( const std::string_view label )
+{
+  return contains<const std::string_view>( label );
+}
+
+Handle<Fix> Server::labeled( const std::string_view label )
+{
+  if ( repository_->contains( label ) ) {
+    return repository_->labeled( label );
+  }
+  return executor_->labeled( label );
 }
--- src/runtime/runtimes.hh
+++ src/runtime/runtimes.hh
@@ -2,6 +2,7 @@
 
 #include "executor.hh"
 #include "handle.hh"
+#include "network.hh"
 #include "repository.hh"
 
 #include <memory>
@@ -33,6 +34,13 @@ public:
   virtual bool contains( const std::string_view label ) override;
   virtual Handle<Fix> labeled( const std::string_view label ) override;
 
+  template<typename T, typename S>
+  std::optional<T> get( Handle<S> name );
+  template<typename T, typename S>
+  void put( Handle<S> name, T data );
+  template<typename T>
+  bool contains( T name );
+
   virtual Handle<Value> execute( Handle<Relation> x ) override;
 };
 
@@ -42,9 +50,75 @@ public:
   ReadWriteRT() {}
 
   static std::shared_ptr<ReadWriteRT> init();
+  virtual Handle<Value> execute( Handle<Relation> x ) override;
+};
+
+class Client : public FrontendRT
+{
+protected:
+  std::optional<Repository> repository_ {};
+  std::optional<NetworkWorker> network_worker_ {};
+  std::shared_ptr<Remote> remote_ {};
+  std::optional<Executor> executor_ {};
+
+public:
+  Client() {}
 
   template<FixType T>
-  void serialize( Handle<T> handle );
+  void send( Handle<T> handle );
+
+  static std::shared_ptr<Client> init( const Address& address );
+
+  virtual std::optional<BlobData> get( Handle<Named> name ) override;
+  virtual std::optional<TreeData> get( Handle<AnyTree> name ) override;
+  virtual std::optional<Handle<Object>> get( Handle<Relation> name ) override;
+  virtual void put( Handle<Named> name, BlobData data ) override;
+  virtual void put( Handle<AnyTree> name, TreeData data ) override;
+  virtual void put( Handle<Relation> name, Handle<Object> data ) override;
+  virtual bool contains( Handle<Named> handle ) override;
+  virtual bool contains( Handle<AnyTree> handle ) override;
+  virtual bool contains( Handle<Relation> handle ) override;
+  virtual bool contains( const std::string_view label ) override;
+  virtual Handle<Fix> labeled( const std::string_view label ) override;
+
+  template<typename T, typename S>
+  std::optional<T> get( Handle<S> name );
+  template<typename T, typename S>
+  void put( Handle<S> name, T data );
+  template<typename T>
+  bool contains( T name );
 
   virtual Handle<Value> execute( Handle<Relation> x ) override;
+};
+
+class Server : public IRuntime
+{
+protected:
+  std::optional<Repository> repository_ {};
+  std::optional<NetworkWorker> network_worker_ {};
+  std::optional<Executor> executor_ {};
+
+public:
+  Server() {}
+
+  static std::shared_ptr<Server> init( const Address& address );
+
+  virtual std::optional<BlobData> get( Handle<Named> name ) override;
+  virtual std::optional<TreeData> get( Handle<AnyTree> name ) override;
+  virtual std::optional<Handle<Object>> get( Handle<Relation> name ) override;
+  virtual void put( Handle<Named> name, BlobData data ) override;
+  virtual void put( Handle<AnyTree> name, TreeData data ) override;
+  virtual void put( Handle<Relation> name, Handle<Object> data ) override;
+  virtual bool contains( Handle<Named> handle ) override;
+  virtual bool contains( Handle<AnyTree> handle ) override;
+  virtual bool contains( Handle<Relation> handle ) override;
+  virtual bool contains( const std::string_view label ) override;
+  virtual Handle<Fix> labeled( const std::string_view label ) override;
+
+  template<typename T, typename S>
+  std::optional<T> get( Handle<S> name );
+  template<typename T, typename S>
+  void put( Handle<S> name, T data );
+  template<typename T>
+  bool contains( T name );
 };
