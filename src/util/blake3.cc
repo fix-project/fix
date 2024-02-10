3b8478583b39d8c0b47ae656c42932565ac8b06f
Matthew Vilaysack <42940122+matthewvilaysack@users.noreply.github.com>
2024-02-09 16:24:53 -0800

    Switch from SHA-256 to BLAKE3 (#156)

--- src/util/blake3.cc
+++ src/util/blake3.cc
@@ -6,13 +6,15 @@
 using namespace std;
 
 namespace blake3 {
-void encode( std::string_view input, u8x32& output )
+u8x32 encode( std::span<const byte> input )
 {
+  u8x32 output;
   array<uint8_t, BLAKE3_OUT_LEN> tmp;
   blake3_hasher hasher;
   blake3_hasher_init( &hasher );
   blake3_hasher_update( &hasher, input.data(), input.size() );
   blake3_hasher_finalize( &hasher, tmp.data(), BLAKE3_OUT_LEN );
   memcpy( &output, tmp.data(), BLAKE3_OUT_LEN );
+  return output;
 }
 }
