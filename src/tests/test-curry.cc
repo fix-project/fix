75a7a71b155c2c5a553f841bbddf291c887fbc7c
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2024-04-08 14:44:16 -0700

    fix curry test

--- src/tests/test-curry.cc
+++ src/tests/test-curry.cc
@@ -34,28 +34,9 @@ Handle<Value> apply_args( Handle<Value> curried, const initializer_list<Handle<F
   return curried;
 }
 
-optional<uint32_t> unwrap_tag( Handle<Fix> tag )
+optional<uint32_t> unwrap_result( Handle<Fix> result )
 {
-  auto h = handle::extract<ExpressionTree>( tag )
-             .transform( []( auto h ) -> Handle<AnyTree> { return h; } )
-             .or_else( [&]() -> optional<Handle<AnyTree>> { return handle::extract<ObjectTree>( tag ); } )
-             .or_else( [&]() -> optional<Handle<AnyTree>> { return handle::extract<ValueTree>( tag ); } );
-
-  if ( !h.has_value() or !h->visit<bool>( []( auto h ) { return h.is_tag(); } ) ) {
-    cout << "unwrap_tag: Expected tag, got non-tag " << tag << endl;
-    return {};
-  }
-
-  auto tag_state = tester::rt->get( h.value() ).value()->at( 2 );
-  if ( handle::extract<Literal>( tag_state ).value() != Handle<Literal>( static_cast<uint32_t>( true ) ) ) {
-    cout << "unwrap_tag: Tag was not successful " << tag_state << endl;
-    return {};
-  }
-
-  auto tag_contents = tester::rt->get( h.value() ).value()->at( 1 );
-  uint32_t blob_result = -1;
-  memcpy( &blob_result, handle::extract<Literal>( tag_contents ).value().data(), sizeof( uint32_t ) );
-  return blob_result;
+  return handle::extract<Literal>( result ).transform( []( auto x ) { return uint32_t( x ); } );
 }
 
 void test_add_simple()
@@ -63,7 +44,7 @@ void test_add_simple()
   auto curried = curry( add_simple_compiled, Handle<Literal>( 2 ) );
   auto curried_result = apply_args( curried, { Handle<Literal>( 1 ), Handle<Literal>( 3 ) } );
 
-  auto optional_result = unwrap_tag( curried_result );
+  auto optional_result = unwrap_result( curried_result );
   if ( not optional_result ) {
     printf( "test_add_simple: Result was not successful\n" );
     exit( 1 );
@@ -79,7 +60,7 @@ void test_add_as_encode()
 
   auto curried_result = apply_args( curried, { Handle<Literal>( 1 ), Handle<Literal>( 3 ) } );
 
-  auto optional_result = unwrap_tag( curried_result );
+  auto optional_result = unwrap_result( curried_result );
   if ( not optional_result ) {
     printf( "test_add_as_encode: Result was not successful\n" );
     exit( 1 );
@@ -95,14 +76,9 @@ void test_curry_self()
 
   auto curried_curry_result = apply_args( curried, { add_simple_compiled, Handle<Literal>( 2 ) } );
 
-  auto unwrapped_curried_curry_result
-    = handle::extract<Value>(
-        tester::rt->get( curried_curry_result.try_into<ValueTree>().value() ).value()->at( 1 ) )
-        .value();
-  auto curried_add_result
-    = apply_args( unwrapped_curried_curry_result, { Handle<Literal>( 1 ), Handle<Literal>( 3 ) } );
+  auto curried_add_result = apply_args( curried_curry_result, { Handle<Literal>( 1 ), Handle<Literal>( 3 ) } );
 
-  auto optional_result = unwrap_tag( curried_add_result );
+  auto optional_result = unwrap_result( curried_add_result );
   if ( not optional_result ) {
     printf( "test_curry_self: Result was not successful\n" );
     exit( 1 );
