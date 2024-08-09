03db64e597525352e770c7e6ebd5ea0c4c535d2f
Yuhan Deng <yhdeng@stanford.edu>
2024-08-09 15:05:52 -0700

    Add create_selection_thunk

--- src/runtime/fixpointapi.hh
+++ src/runtime/fixpointapi.hh
@@ -41,7 +41,7 @@ u8x32 create_application_thunk( u8x32 handle );
 // Return Handle<Identification>, traps if handle is not Handle<Value>
 u8x32 create_identification_thunk( u8x32 handle );
 
-// Return Handle<Selection>, traps if handle is not Handle<ObjectTree>
+// Return Handle<Selection>, traps if handle is not Handle<Object>
 u8x32 create_selection_thunk( u8x32 handle, uint32_t idx );
 
 // Traps if handle is not Handle<AnyTree> or Handle<AnyTreeRef> or Handle<Blob> or Handle<BlobRef>
